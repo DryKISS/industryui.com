3ee0755acbffa18dac34aa97bffd5186066f5008
Alex_Y <yegupov@gmail.com>
2018-12-14 12:58:47 +0200

    added Columns sizes & offsets story

--- public/components/atoms/grid/stories.js
+++ public/components/atoms/grid/stories.js
@@ -11,8 +11,10 @@ import { withInfo } from '@storybook/addon-info'
 import { withReadme } from 'storybook-readme'
 
 // UI
-import { Container } from '../../'
+import { Container, Row, Column } from '../../'
 import Readme from './README.md'
+
+import { COLUMNS } from './__mocks__'
 
 storiesOf('Atoms/Grid', module)
 
@@ -21,5 +23,40 @@ storiesOf('Atoms/Grid', module)
   .add('Default',
     withInfo()(() =>
       <Container />
+    )
+  )
+
+  .add('Columns sizes & offsets',
+       withInfo()(() => {
+         const style = {
+           border: '1px gray solid',
+         }
+         
+         return (
+           <Container>
+             <Row>
+               <Column md={4} style={style}>
+                 {`md={4}`}
+               </Column>
+               <Column style={style} md={4} offset={{ md: 4 }} style={style} >
+                 {`md={4} offset={{md:4}}`}
+               </Column>
+             </Row>
+             <Row>
+               <Column md={3} offset={{ md: 3 }} style={style} >
+                 {`md={3} offset={{md:3}} `}
+               </Column>
+               <Column md={3} offset={{ md: 3 }} style={style} >
+                 {`md={3} offset={{md:3}}`}
+               </Column>
+             </Row>
+             <Row>
+               <Column md={6} offset={{ md: 3 }} style={style} >
+                 {`md={6} offset={{md:3}}`}
+               </Column>
+             </Row>
+           </Container>
+         )
+       }
     )
   )
