db910079377870d8261117600e6d764047d47a1f
Alex_Y <yegupov@gmail.com>
2018-12-14 13:37:20 +0200

    added simple grid story

--- public/components/atoms/grid/stories.js
+++ public/components/atoms/grid/stories.js
@@ -16,46 +16,63 @@ import Readme from './README.md'
 
 //import { COLUMNS } from './__mocks__'
 
+const colStyle = { border: '1px gray solid' }
+
 storiesOf('Atoms/Grid', module)
 
   .addDecorator(withReadme(Readme))
 
-  .add('Default',
-    withInfo()(() =>
-      <Container />
-    )
-  )
+  .add('Default', withInfo()(() =>
+    <Container>
+      <Row>
+        <Column>
+          11
+        </Column>
+        <Column>
+          12
+        </Column>
+      </Row>
+      <Row >
+        <Column>
+          21
+        </Column>
+        <Column>
+          22
+        </Column>
+        <Column>
+          23
+        </Column>
+      </Row>
+      <Row>
+        <Column>
+          31
+        </Column>
+      </Row>
+    </Container>
+  ))
 
-  .add('Columns sizes & offsets', withInfo()(() => {
-    const style = {
-      border: '1px gray solid'
-    }
-
-    return (
-      <Container>
-        <Row>
-          <Column md={4} style={style}>
-            {`md={4}`}
-          </Column>
-          <Column style={style} md={4} offset={{ md: 4 }} style={style} >
-            {`md={4} offset={{md:4}}`}
-          </Column>
-        </Row>
-        <Row>
-          <Column md={3} offset={{ md: 3 }} style={style} >
-            {`md={3} offset={{md:3}} `}
-          </Column>
-          <Column md={3} offset={{ md: 3 }} style={style} >
-            {`md={3} offset={{md:3}}`}
-          </Column>
-        </Row>
-        <Row>
-          <Column md={6} offset={{ md: 3 }} style={style} >
-            {`md={6} offset={{md:3}}`}
-          </Column>
-        </Row>
-      </Container>
-    )
-  }
-  )
-  )
+  .add('With column sizes & offsets', withInfo()(() =>
+    <Container>
+      <Row>
+        <Column md={4} style={colStyle}>
+          {`md=4`}
+        </Column>
+        <Column style={colStyle} md={4} offset={{ md: 4 }} style={colStyle} >
+          {`md=4 offset={md:4}`}
+        </Column>
+      </Row>
+      <Row>
+        <Column md={3} offset={{ md: 3 }} style={colStyle} >
+          {`md=3 offset={md:3}`}
+        </Column>
+        <Column md={3} offset={{ md: 3 }} style={colStyle} >
+          {`md=3 offset={md:3}`}
+        </Column>
+      </Row>
+      <Row>
+        <Column md={6} offset={{ md: 3 }} style={colStyle} >
+          {`md=6 offset={md:3}`}
+        </Column>
+      </Row>
+    </Container>
+  ))
