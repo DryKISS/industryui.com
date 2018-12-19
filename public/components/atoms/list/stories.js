4efb7380908173c62450ef00d245113f5e87e411
Alex_Y <yegupov@gmail.com>
2018-12-19 19:15:04 +0200

    added knobs to list

--- public/components/atoms/list/stories.js
+++ public/components/atoms/list/stories.js
@@ -9,12 +9,14 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 import { withReadme } from 'storybook-readme'
+import { withKnobs, boolean, object, text } from '@storybook/addon-knobs/react'
 
 import { List, ListItem } from '../../'
 import Readme from './README.md'
 
 storiesOf('Atoms/List', module)
 
+  .addDecorator(withKnobs)
   .addDecorator(withReadme(Readme))
 
   .add('default', withInfo()(() =>
@@ -26,8 +28,12 @@ storiesOf('Atoms/List', module)
   )
   )
 
-  .add('inline', withInfo()(() =>
-    <List inline>
+  .add('knobbed', withInfo()(() =>
+    <List
+      unstyled={boolean('Unstyled', false)}
+      inline={boolean('inline', false)}
+      style={object('Style', { listStyleType: 'lower-greek' })}
+    >
       <ListItem>Item1</ListItem>
       <ListItem>Item2</ListItem>
       <ListItem>Item3</ListItem>
@@ -35,20 +41,3 @@ storiesOf('Atoms/List', module)
   )
   )
 
-  .add('unstyled', withInfo()(() =>
-    <List unstyled>
-      <ListItem>Item1</ListItem>
-      <ListItem>Item2</ListItem>
-      <ListItem>Item3</ListItem>
-    </List>
-  )
-  )
-
-  .add('custom styled', withInfo()(() =>
-    <List style={{ listStyleType: 'lower-greek' }}>
-      <ListItem>Item1</ListItem>
-      <ListItem>Item2</ListItem>
-      <ListItem>Item3</ListItem>
-    </List>
-  )
-  )
