2b5ddadd279ee391efca8e7e5483a66363939227
Kristian Heruc <kristian@heruc.com>
2019-09-26 14:48:25 +0200

    Add mock data

---
+++ components/molecules/imageLocation/__mocks__/properties.js
@@ -0,0 +1,62 @@
+/**
+ * Properties
+ */
+
+export const Properties = [
+
+  {
+    id: 1,
+    name: "10 Downing street",
+    floors: [
+      {
+        id: 1,
+        name: "Ground Floor",
+        image: "ground-floor.png"
+      },
+      {
+        id: 2,
+        name: "First Floor",
+        image: "first-floor.png"
+      }
+    ]
+  },
+
+  {
+    id: 2,
+    name: "23 Acacia Road",
+    floors: [
+
+      {
+        id: 1,
+        name: "Ground Floor",
+        image: "ground-floor.png"
+      },
+
+      {
+        id: 2,
+        name: "First Floor",
+        image: "first-floor.png"
+      },
+
+      {
+        id: 3,
+        name: "Second Floor",
+        image: "second-floor.png"
+      },
+
+      {
+        id: 4,
+        name: "Third Floor",
+        image: "third-floor.png"
+      },
+
+      {
+        id: 5,
+        name: "Fourth Floor",
+        image: "fourth-floor.png"
+      }
+    ]
+
+  },
+
+]
