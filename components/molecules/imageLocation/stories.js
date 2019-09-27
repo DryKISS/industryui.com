1ba28945c9ca4131490822aea835a539f3012e7a
Kristian Heruc <kristian@heruc.com>
2019-09-27 15:49:49 +0200

    Update stories

--- components/molecules/imageLocation/stories.js
+++ components/molecules/imageLocation/stories.js
@@ -3,13 +3,16 @@
  */
 
 // React
-import React from 'react'
+import React, { useState } from 'react'
+
+import styled from 'styled-components'
 
 // Storybook
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 
 // UI
+import { Select } from '../../../'
 import { ImageLocation } from '../../'
 import Readme from './README.md'
 
@@ -23,6 +26,28 @@ import secondFloor from './__resources__/images/second-floor.png'
 import thirdFloor from './__resources__/images/third-floor.png'
 import fourthFloor from './__resources__/images/fourth-floor.png'
 
+const PropertySelect = ({ properties, change }) => {
+  const [property, setProperty] = useState(null);
+
+  const handlePropertyChange = (event) => {
+    const selected = properties.find(p => p.value == event.target.value) || null;
+    setProperty(selected);
+  }
+
+  return (
+    <StyledPropertySelect>
+      <Select label='Property' options={[{ text: 'Select property', value: ''}, ...properties]} change={handlePropertyChange} />
+      {property && <ImageLocation key={property.value} label='Floor' options={property.options} change={change} />}
+    </StyledPropertySelect>
+  );
+};
+
+const StyledPropertySelect = styled.div`
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  border-radius: 0.25rem;
+  padding: 20px;
+`
+
 storiesOf('Molecules/ImageLocation', module)
   .addParameters({
     readme: {
@@ -31,6 +56,12 @@ storiesOf('Molecules/ImageLocation', module)
   })
 
   .add('Default', () => (
-    <ImageLocation properties={Properties} change={action('position-change')}></ImageLocation>
+    <ImageLocation options={Properties[1].options} label='Option' change={action('position-change')} />
   ))
+
+  .add('Multiple properties', () => (
+    <PropertySelect properties={Properties} change={action('position-change')} />
+  ))
+
+
 
