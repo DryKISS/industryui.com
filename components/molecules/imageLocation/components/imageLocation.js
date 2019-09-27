f65e1728a71b2cf1221f0fd95458c8a64a9f1af2
Kristian Heruc <kristian@heruc.com>
2019-09-27 15:49:31 +0200

    Refactor ImageLocation component

--- components/molecules/imageLocation/components/imageLocation.js
+++ components/molecules/imageLocation/components/imageLocation.js
@@ -1,53 +1,44 @@
 /**
- * ImageLocation
+ * Image Location
  */
 
 // React
 import React, { useState } from 'react'
-import { array, func, string, object  } from 'prop-types'
+import { array, func, string, object } from 'prop-types'
 
 import styled from 'styled-components'
 
 // UI
 import { Select } from '../../../'
-import { FloorWrapper } from '../../'
+import { ImageWrapper } from '../../'
 
-export const ImageLocation = ({ properties, change }, props) => {
+export const ImageLocation = ({ change, label, options }, props) => {
+  const [item, setItem] = useState(null);
 
-  const [property, setProperty] = useState(null);
-
-  const options = [{ text: 'Select property', value: ''}];
-
-  properties.map(p => {
-    options.push({
-      text: p.name,
-      value: p.id
-    });
-  });
-
-  const handlePropertyChange = (event) => {
-    const selected = properties.find(p => p.id == event.target.value) || null;
-    setProperty(selected);
+  const handleItemChange = (event) => {
+    const selected = options.find(o => o.value == event.target.value) || null;
+    setItem(selected);
   }
 
   return (
-    <LocationWrapper {...props} >
-      <Select label="Property" options={options} change={handlePropertyChange} />
-      {property && <FloorWrapper key={property.id} property={property} change={change} />}
-    </LocationWrapper>
-  );
-};
+    <StyledImageLocation {...props}>
+      <Select
+      change={handleItemChange}
+      label={label}
+      options={[{ text: `Select ${label}`, value: ''}, ...options]}
+      />
+      {item && <ImageWrapper item={item} change={change} />}
+    </StyledImageLocation>
+  )
+}
 
-const LocationWrapper = styled.div`
-  border: 1px solid rgba(0, 0, 0, 0.125);
-  border-radius: 0.25rem;
-  padding: 20px;
+const StyledImageLocation = styled.div`
   overflow-x: auto;
 `
 
 ImageLocation.propTypes = {
   change: func.isRequired,
   className: string,
-  properties: array.isRequired,
+  options: array.isRequired,
   style: object
 }
