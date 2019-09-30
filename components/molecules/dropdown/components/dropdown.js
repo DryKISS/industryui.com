b644e5b285227cdcc0ed36317f1bfdfb3684f146
Kristian Heruc <kristian@heruc.com>
2019-09-30 16:36:18 +0200

    Add a Button with an Icon and a toggle state

--- components/molecules/dropdown/components/dropdown.js
+++ components/molecules/dropdown/components/dropdown.js
@@ -2,19 +2,22 @@
  * Dropdown
  */
 
-import React from 'react'
+import React, { useState } from 'react'
 
 import { node, object, string, array } from 'prop-types'
 
 import styled from 'styled-components'
 
 import { DropdownMenu } from '../../'
+import { Button, Icon } from '../../../'
 
 export const Dropdown = ({ children, className, items, style, title }) => {
+  const [open, setOpen] = useState(false)
+
   return (
     <StyledDropdown className={className} style={style}>
-      {title}
-      {children || <DropdownMenu items={items} />}
+      <Button onClick={() => setOpen(!open)}>{title} <Icon aria-hidden="true" context="info" icon='caret-down' /></Button>
+      {open && (children || <DropdownMenu items={items} />)}
     </StyledDropdown>
   );
 };
