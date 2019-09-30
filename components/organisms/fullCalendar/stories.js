067947acd6e90f003ed651fbbaf9129e098f479a
Kristian Heruc <kristian@heruc.com>
2019-09-30 12:20:52 +0200

    Update stories

--- components/organisms/fullCalendar/stories.js
+++ components/organisms/fullCalendar/stories.js
@@ -9,6 +9,9 @@ import { storiesOf } from '@storybook/react'
 import { Calendar } from './'
 import Readme from './README.md'
 
+// Data
+import { Events } from './__mocks__/events'
+
 storiesOf('Organisms/Full Calendar', module)
 
   .addParameters({
@@ -19,4 +22,8 @@ storiesOf('Organisms/Full Calendar', module)
 
   .add('Default', () =>
     <Calendar />
+  )
+
+  .add('With Events', () =>
+    <Calendar events={Events} />
   )
