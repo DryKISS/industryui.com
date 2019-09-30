835569e46f47e4b2007c152fe46b2b2338346b28
Kristian Heruc <kristian@heruc.com>
2019-09-30 16:01:06 +0200

    Add stories

---
+++ components/molecules/dropdown/stories.js
@@ -0,0 +1,28 @@
+/**
+ * Dropdown
+ */
+
+// React
+import React from 'react'
+
+// Storybook
+import { storiesOf } from '@storybook/react'
+
+// UI
+import { Dropdown } from '../../'
+import Readme from './README.md'
+
+// Data
+import { Items } from './__mocks__/items'
+
+storiesOf('Molecules/Dropdown', module)
+
+  .addParameters({
+    readme: {
+      sidebar: Readme
+    }
+  })
+
+  .add('Default', () =>
+    <Dropdown items={Items} title='Dropdown' />
+  )
