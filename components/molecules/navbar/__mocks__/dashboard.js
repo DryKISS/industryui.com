ff1eddb2846b074d2702ad6ebb1839db5fdfb972
Kristian Heruc <kristian@heruc.com>
2019-10-01 16:08:21 +0200

    Add mock data

---
+++ components/molecules/navbar/__mocks__/dashboard.js
@@ -0,0 +1,22 @@
+/**
+ * Dashboard
+ */
+export const Dashboard = {
+
+  right: [
+
+    {
+      id: 'navDashboard',
+      name: 'Dashboard',
+      to: '/dashboard'
+    },
+
+    {
+      id: 'navIssue',
+      name: 'Report Issue',
+      to: '/dashboard/issues/create'
+    }
+
+  ]
+
+}
