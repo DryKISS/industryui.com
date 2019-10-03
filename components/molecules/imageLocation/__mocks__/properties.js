87952abcd998ebfc71f5a002b7c6dde54f9e6bab
Kristian Heruc <kristian@heruc.com>
2019-10-03 17:14:40 +0200

    Update image paths

--- components/molecules/imageLocation/__mocks__/properties.js
+++ components/molecules/imageLocation/__mocks__/properties.js
@@ -2,23 +2,23 @@
  * Properties
  */
 
-export const Properties =  [
+export const Properties = [
 
   {
     value: 1,
-    text: "10 Downing street",
+    text: '10 Downing street',
     options: [
 
       {
         value: 1,
-        text: "Ground Floor",
-        image: "/static/media/ground-floor.e15d4aa1.png"
+        text: 'Ground Floor',
+        image: '/static/media/ground-floor.e5fb167c.png'
       },
 
       {
         value: 2,
-        text: "First Floor",
-        image: "/static/media/first-floor.0361b1ac.png"
+        text: 'First Floor',
+        image: '/static/media/first-floor.22d20f1f.png'
       }
 
     ]
@@ -27,41 +27,41 @@ export const Properties =  [
 
   {
     value: 2,
-    text: "23 Acacia Road",
+    text: '23 Acacia Road',
     options: [
 
       {
         value: 1,
-        text: "Ground Floor",
-        image: "/static/media/ground-floor.e15d4aa1.png"
+        text: 'Ground Floor',
+        image: '/static/media/ground-floor.e5fb167c.png'
       },
 
       {
         value: 2,
-        text: "First Floor",
-        image: "/static/media/first-floor.0361b1ac.png"
+        text: 'First Floor',
+        image: '/static/media/first-floor.22d20f1f.png'
       },
 
       {
         value: 3,
-        text: "Second Floor",
-        image: "/static/media/second-floor.69f937cb.png"
+        text: 'Second Floor',
+        image: '/static/media/second-floor.523f86bc.png'
       },
 
       {
         value: 4,
-        text: "Third Floor",
-        image: "/static/media/third-floor.b0d19213.png"
+        text: 'Third Floor',
+        image: '/static/media/third-floor.fb1270b1.png'
       },
 
       {
         value: 5,
-        text: "Fourth Floor",
-        image: "/static/media/fourth-floor.42dabae3.png"
+        text: 'Fourth Floor',
+        image: '/static/media/fourth-floor.91e398b1.png'
       }
 
     ]
 
-  },
+  }
 
 ]
