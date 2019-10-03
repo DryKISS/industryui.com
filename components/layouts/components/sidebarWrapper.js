11a126188f272e63b510c7013b545074f5a8ef77
Kristian Heruc <kristian@heruc.com>
2019-10-03 13:23:34 +0200

    Sidebar updates (#26)
    * Make sidebar wrapper an aside element
    * Remove border and background from Sidebar
    * Update sidebar styles
    * Remove padding from fluidWrapper
    * Remove footer border
    * Remove Dashboard page
    * Update content wrapper padding

--- components/layouts/components/sidebarWrapper.js
+++ components/layouts/components/sidebarWrapper.js
@@ -13,13 +13,24 @@ export const SidebarWrapper = ({ children, className, style }) => {
     <StyledSidebarWrapper className={className} style={style}>
       {children}
     </StyledSidebarWrapper>
-  );
-};
+  )
+}
 
-const StyledSidebarWrapper = styled.div`
+const StyledSidebarWrapper = styled.aside`
   align-self: stretch;
+  background: #001529;
+  border-right: 1px solid #eef1f4;
+  box-shadow: 0 0 50px -20px rgba(57,55,73,.4);
   flex: 0 0 250px;
   padding: 20px 10px;
+
+  a {
+    color: #b8beca;
+    line-height: 2;
+    &:hover {
+      color: #fff;
+    }
+  }
 `
 
 SidebarWrapper.propTypes = {
