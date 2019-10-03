c04cc9693b9c102033b90eedc40e81538cb3d1cf
Kristian Heruc <kristian@heruc.com>
2019-10-03 13:06:22 +0200

    Update sidebar styles

--- components/layouts/components/sidebarWrapper.js
+++ components/layouts/components/sidebarWrapper.js
@@ -18,8 +18,19 @@ export const SidebarWrapper = ({ children, className, style }) => {
 
 const StyledSidebarWrapper = styled.aside`
   align-self: stretch;
+  background: #001529;
+  border-right: 1px solid #eef1f4;
+  box-shadow: 0 0 50px -20px rgba(57,55,73,.4);
   flex: 0 0 250px;
   padding: 20px 10px;
+
+  a {
+    color: #b8beca;
+    line-height: 2;
+    &:hover {
+      color: #fff;
+    }
+  }
 `
 
 SidebarWrapper.propTypes = {
