548340c8899ff97b59bc8fd97ed223653ea9ddd6
Kristian Heruc <kristian@heruc.com>
2019-10-08 09:21:17 +0200

    Update FullCalendar mock data & examples (#34)
    * Update mock data
    * Add click event handler to the story
    * Update Readme

--- components/organisms/fullCalendar/stories.js
+++ components/organisms/fullCalendar/stories.js
@@ -12,6 +12,13 @@ import Readme from './README.md'
 // Data
 import { Events } from './__mocks__/events'
 
+const handleEventClick = ({ event }) => {
+  console.log('Event', event)
+  console.log(`Clicked: ${event.title} - ${event.extendedProps.description}`)
+  // update event title
+  event.setProp('title', event.title + ' - ' + 'updated')
+}
+
 storiesOf('Organisms/Full Calendar', module)
 
   .addParameters({
@@ -26,4 +33,8 @@ storiesOf('Organisms/Full Calendar', module)
 
   .add('With Events', () =>
     <Calendar events={Events} />
+  )
+
+  .add('Event Click', () =>
+    <Calendar eventClick={handleEventClick} events={Events} />
   )
