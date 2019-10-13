d9b435730e0167c9846eed01c38355c3e9f000a1
Ian Warner <ian.warner@drykiss.com>
2019-10-14 00:44:33 +0200

    fix: remove static and add gitignore

--- storybook-static/main.ebf3abaaef64be3599f0.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(444),c(552),n.exports=c(982)},1:function(n,o){},552:function(n,o,c){"use strict";c.r(o);c(553),c(713),c(830),c(940),c(943)}},[[0,1,2]]]);
