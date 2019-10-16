ea30e8d8652f4710cb008f4b8f6391896955766f
Kristian Heruc <kristian@heruc.com>
2019-10-16 17:56:37 +0200

    Add auth utils for generating and validating tokens

--- components/utils/auth.js
+++ components/utils/auth.js
@@ -3,6 +3,7 @@
  */
 
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const hashPassword = (password) => {
   return bcrypt.hashSync(password, 10)
@@ -12,3 +13,10 @@ export const validatePassword = (user, password) => {
   return bcrypt.compareSync(password, user.password)
 }
 
+export const generateToken = (data, secret) => {
+  return jwt.sign(data, secret, { expiresIn: '14d' });
+}
+
+export const validateToken = (token, secret) => {
+  return jwt.verify(token, secret)
+}
