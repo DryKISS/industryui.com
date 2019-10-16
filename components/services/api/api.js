9370a545e0236b7a1a87fd7cdd58222e9a582a14
Kristian Heruc <kristian@heruc.com>
2019-10-16 15:37:20 +0200

    Add init method for the api service

--- components/services/api/api.js
+++ components/services/api/api.js
@@ -10,17 +10,21 @@ import { apiConfig } from 'config'
 
 const http = axios.create(apiConfig)
 
-// Mock requests
-const mocker = require('api/client.mock')
-mocker.apply(http)
-
 export const api = {
 
+  init: () => {
+    // Mock requests
+    const mocker = require('api/client.mock')
+    mocker.apply(http)
+  },
+
   get: async(url) => {
+    init()
     return http.get(url)
   },
 
   post: async(url, data) => {
+    init()
     return http.post(url, data)
   }
 
