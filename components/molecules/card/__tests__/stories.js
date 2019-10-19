8e9cda2be46a150f2d73bc26d205c73774f5d90c
Majid Amiri <majid@licenserocks.de>
2019-10-19 15:13:48 +0330

    Migrated Molecules/Card

---
+++ components/molecules/card/__tests__/stories.js
@@ -0,0 +1,78 @@
+/**
+ * Card Stories
+ */
+
+// React
+import React from 'react'
+
+import { withKnobs, boolean, text } from '@storybook/addon-knobs'
+
+// UI
+import { Button, Card, Column } from 'components'
+import Readme from '../README.md'
+import vizla from '../__resources__/vizla.jpg'
+import seeker from '../__resources__/seeker.png'
+
+export default {
+  title: 'Molecules/Card',
+  component: Card,
+  decorators: [story => <Column md={3}>{story()}</Column>, withKnobs],
+  parameters: {
+    readme: {
+      sidebar: Readme
+    }
+  }
+}
+
+export const defaultStory = () => (
+  <Card
+    alt='Vizla'
+    bordered={boolean('Bordered', true)}
+    footer={text('Footer', 'Footer text')}
+    image={vizla}
+    title={text('Title', 'Title of the article')}
+  >
+    {text('Description', 'Description of the card.')}
+  </Card>
+)
+
+defaultStory.story = {
+  name: 'Default'
+}
+
+export const horizontalStory = () => (
+  <Card
+    alt='Vizla'
+    bordered={boolean('Bordered', true)}
+    horizontal={boolean('Horizontal', true)}
+    image={seeker}
+    title={text('Title', 'Are you a dog seeker?')}
+  >
+    <p>
+      {text(
+        'Description',
+        `The wise man therefore always holds in these matters to this
+               principle of selection: he rejects pleasures to secure other
+               greater pleasures, or else he endures.`
+      )}
+    </p>
+
+    <Button className='Card-absoluteBottom' secondary>
+      View available dogs
+    </Button>
+  </Card>
+)
+
+horizontalStory.story = {
+  name: 'Horizontal'
+}
+
+export const breed = () => (
+  <Card alt='Vizla' bordered={boolean('Bordered', true)} footer='Vizsla' image={vizla} />
+)
+
+export const noImage = () => (
+  <Card bordered={boolean('Bordered', false)} title='No Image'>
+    Content
+  </Card>
+)
