82feff9bcb7272c8a688842da89b907c995e6df3
Majid Amiri <majid@licenserocks.de>
2019-10-19 14:06:33 +0330

    Migrated Google

--- components/atoms/google/maps/static/stories.js
+++ components/atoms/google/maps/static/stories.js
@@ -5,32 +5,35 @@
 // React
 import React from 'react'
 
-// Storybook
-import { storiesOf } from '@storybook/react'
-
 // UI
 import { GoogleMapsStatic, GoogleMapsStaticPath } from './'
 import Readme from './README.md'
 
-storiesOf('Atoms/Google/Static Map', module)
-
-  .addParameters({
+export default {
+  title: 'Atoms/Google/Static Map',
+  component: GoogleMapsStatic,
+  parameters: {
     readme: {
       sidebar: Readme
     }
-  })
+  }
+}
 
-  .add('Default', () =>
-    <GoogleMapsStatic size='600x600' apiKey=''>
-      <GoogleMapsStaticPath
-        color='0xff0000ff'
-        weight='5'
-        points={[
-          { lat: 40.737102, lng: -73.990318 },
-          '40.749825,-73.987963',
-          { lat: 40.752946, lng: -73.987384 },
-          { lat: 40.755823, lng: -73.986397 }
-        ]}
-      />
-    </GoogleMapsStatic>
-  )
+export const defaultStory = () => (
+  <GoogleMapsStatic size='600x600' apiKey=''>
+    <GoogleMapsStaticPath
+      color='0xff0000ff'
+      weight='5'
+      points={[
+        { lat: 40.737102, lng: -73.990318 },
+        '40.749825,-73.987963',
+        { lat: 40.752946, lng: -73.987384 },
+        { lat: 40.755823, lng: -73.986397 }
+      ]}
+    />
+  </GoogleMapsStatic>
+)
+
+defaultStory.story = {
+  name: 'Default'
+}
