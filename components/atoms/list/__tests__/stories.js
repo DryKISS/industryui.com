80a2bc56b7dd8023103697e2a77a9db79ef1c98b
Majid Amiri <majid@licenserocks.de>
2019-10-19 14:47:10 +0330

    Migrated List

--- components/atoms/list/__tests__/stories.js
+++ components/atoms/list/__tests__/stories.js
@@ -5,65 +5,80 @@
 // React
 import React from 'react'
 
-// Storybook
-import { storiesOf } from '@storybook/react'
 import { withKnobs, boolean, object } from '@storybook/addon-knobs'
 
 import { List, ListItem } from '../../../'
 import Readme from '../README.md'
 
-storiesOf('Atoms/List', module)
-
-  .addDecorator(withKnobs)
-  .addParameters({
+export default {
+  title: 'Atoms/List',
+  decorators: [withKnobs],
+  parameters: {
     readme: {
       sidebar: Readme
     }
-  })
+  }
+}
 
-  .add('Default', () =>
-    <List>
-      <ListItem>Item1</ListItem>
-      <ListItem>Item2</ListItem>
-      <ListItem>Item3</ListItem>
-    </List>
-  )
+export const defaultStory = () => (
+  <List>
+    <ListItem>Item1</ListItem>
+    <ListItem>Item2</ListItem>
+    <ListItem>Item3</ListItem>
+  </List>
+)
 
-  .add('Group', () =>
-    <List group>
-      <ListItem>Item1</ListItem>
-      <ListItem>Item2</ListItem>
-      <ListItem>Item3</ListItem>
-    </List>
-  )
+defaultStory.story = {
+  name: 'Default'
+}
 
-  .add('Border', () =>
-    <List border>
-      <ListItem>Item1</ListItem>
-      <ListItem>Item2</ListItem>
-      <ListItem>Item3</ListItem>
-    </List>
-  )
+export const groupStory = () => (
+  <List group>
+    <ListItem>Item1</ListItem>
+    <ListItem>Item2</ListItem>
+    <ListItem>Item3</ListItem>
+  </List>
+)
 
-  .add('Flush', () =>
-    <List flush>
-      <ListItem>Item1</ListItem>
-      <ListItem>Item2</ListItem>
-      <ListItem>Item3</ListItem>
-    </List>
-  )
+groupStory.story = {
+  name: 'Group'
+}
 
-  .add('Knobbed', () =>
-    <List
-      border={boolean('border', false)}
-      flush={boolean('flush', false)}
-      group={boolean('group', false)}
-      unstyled={boolean('unstyled', false)}
-      inline={boolean('inline', false)}
-      style={object('Style', { color: 'blue', listStyleType: 'lower-greek' })}
-    >
-      <ListItem>Item1</ListItem>
-      <ListItem>Item2</ListItem>
-      <ListItem>Item3</ListItem>
-    </List>
-  )
+export const borderStory = () => (
+  <List border>
+    <ListItem>Item1</ListItem>
+    <ListItem>Item2</ListItem>
+    <ListItem>Item3</ListItem>
+  </List>
+)
+
+borderStory.story = {
+  name: 'Border'
+}
+
+export const flushStory = () => (
+  <List flush>
+    <ListItem>Item1</ListItem>
+    <ListItem>Item2</ListItem>
+    <ListItem>Item3</ListItem>
+  </List>
+)
+
+flushStory.story = {
+  name: 'Flush'
+}
+
+export const knobbed = () => (
+  <List
+    border={boolean('border', false)}
+    flush={boolean('flush', false)}
+    group={boolean('group', false)}
+    unstyled={boolean('unstyled', false)}
+    inline={boolean('inline', false)}
+    style={object('Style', { color: 'blue', listStyleType: 'lower-greek' })}
+  >
+    <ListItem>Item1</ListItem>
+    <ListItem>Item2</ListItem>
+    <ListItem>Item3</ListItem>
+  </List>
+)
