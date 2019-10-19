eea5a342bc522ba7154274a2db3e7520a72808a1
Majid Amiri <majid@licenserocks.de>
2019-10-19 16:25:00 +0330

    Migrated Organisms/Login

---
+++ components/organisms/login/__tests__/stories.js
@@ -0,0 +1,28 @@
+/**
+ * Login
+ */
+
+// React
+import React from 'react'
+
+// UI
+import { Login } from '../'
+import Readme from '../README.md'
+
+export default {
+  title: 'Organisms/Login',
+  component: Login,
+  parameters: {
+    readme: {
+      sidebar: Readme
+    }
+  }
+}
+
+export const defaultStory = () => (
+  <Login change={() => {}} email='' submit={() => {}} password='' />
+)
+
+defaultStory.story = {
+  name: 'Default'
+}
