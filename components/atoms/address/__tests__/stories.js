872b22734cf4ae7f92e956f358b029d4276eab6d
Majid Amiri <45729098+majid-amiri@users.noreply.github.com>
2019-10-19 12:47:37 +0330

    Changed Address stories to CSF using migration tool (#49)

--- components/atoms/address/__tests__/stories.js
+++ components/atoms/address/__tests__/stories.js
@@ -1,33 +1,36 @@
 /**
- * Avatar
+ * Address
  */
 
 // React
 import React from 'react'
 
-// Storybook
-import { storiesOf } from '@storybook/react'
-
 // UI
 import { Address } from '../'
 import Readme from '../README.md'
 
-storiesOf('Atoms/Address', module)
-
-  .addParameters({
+export default {
+  title: 'Atoms/Address',
+  component: Address,
+  parameters: {
     readme: {
       sidebar: Readme
     }
-  })
+  }
+}
 
-  .add('Default', () =>
-    <Address
-      county='Sussex'
-      country='United Kingdom'
-      line1='24 Accacia Road'
-      line2='Grandhouse'
-      line3='Bermondsey'
-      postcode='W1 2N'
-      town='London'
-    />
-  )
+export const defaultStory = () => (
+  <Address
+    county='Sussex'
+    country='United Kingdom'
+    line1='24 Accacia Road'
+    line2='Grandhouse'
+    line3='Bermondsey'
+    postcode='W1 2N'
+    town='London'
+  />
+)
+
+defaultStory.story = {
+  name: 'Default'
+}
