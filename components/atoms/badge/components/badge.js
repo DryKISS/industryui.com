32c35ace5a49d3684576f8c89b0da351fc45304f
Majid Amiri <majidamiriii@gmail.com>
2019-10-25 00:52:55 +0330

    Add icon, folder structure, imporve stories, proptypes

---
+++ components/atoms/badge/components/badge.js
@@ -0,0 +1,57 @@
+/**
+ * Badge
+ */
+
+// React
+import { string, oneOf, object } from 'prop-types'
+import { CONTEXT, Icon } from 'components'
+
+// Style
+import styled from 'styled-components'
+
+export const Badge = ({ className, content, style, to, context, icon }) => {
+  return (
+    <StyledBadge
+      className={className}
+      itemProp='keywords'
+      href={to}
+      style={style}
+      context={context}
+    >
+      {icon && <Icon icon={icon} style={{ marginRight: '5px' }} />}
+      {content}
+    </StyledBadge>
+  )
+}
+
+const StyledBadge = styled.a`
+  background-color: ${props => props.theme.COLOUR.dark};
+  color: ${props => props.theme.COLOUR.white};
+  border-radius: 0;
+  display: inline-block;
+  font-size: 90%;
+  line-height: 1;
+  margin: 0 .5em .5em 0;
+  padding: .5em;
+  text-align: center;
+  vertical-align: baseline;
+  white-space: nowrap;
+
+  &:hover {
+    background-color: ${props => props.theme.COLOUR.dark};
+    color: ${props => props.theme.COLOUR.light};;
+  }
+
+  &:empty {
+    display: none;
+  }
+`
+
+Badge.propTypes = {
+  className: string,
+  style: object,
+  content: string.isRequired,
+  to: string,
+  context: oneOf(Object.values(CONTEXT)),
+  icon: object
+}
