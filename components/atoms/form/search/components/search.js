67c7ea6efc9e402e4e8ae6d8323bcc28fc7677e8
Majid Amiri <45729098+majid-amiri@users.noreply.github.com>
2019-10-25 12:51:57 +0330

    Modified structure and added search type (#55)
    * Modified structure and added search type
    
    * Fixed 80 chars format

---
+++ components/atoms/form/search/components/search.js
@@ -0,0 +1,69 @@
+/**
+ * Search
+ *
+ */
+
+// React
+import { func, string, oneOf } from 'prop-types'
+
+// UI
+import { Button, useChange, Form, Input } from 'components'
+
+// Style
+import styled from 'styled-components'
+
+export const Search = ({ label, onSearch, placeholder, value, type }) => {
+  const INITIAL_STATE = {
+    query: value || ''
+  }
+
+  const [change, form] = useChange(INITIAL_STATE)
+  const { query } = form
+
+  return (
+    <Form submit={() => onSearch(query)}>
+      <StyledSearch>
+        <Input
+          change={change}
+          id='query'
+          placeholder={placeholder}
+          required={false}
+          type={type}
+          value={query}
+        />
+        <Button
+          content={label || 'Search'}
+          context='dark'
+          size='lg'
+          type='submit'
+        />
+      </StyledSearch>
+    </Form>
+  )
+}
+
+const StyledSearch = styled.div`
+  display: flex;
+
+  > input {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+
+  > button {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+`
+
+Search.propTypes = {
+  label: string,
+  onSearch: func.isRequired,
+  placeholder: string,
+  value: string,
+  type: oneOf(['search', 'text'])
+}
+
+Search.defaultProps = {
+  type: 'search'
+}
