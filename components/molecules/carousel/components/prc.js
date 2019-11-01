4a9de497b48cf8c7adbaf8aee76f3bd837b9b902
Majid Amiri <majidamiriii@gmail.com>
2019-11-01 16:36:06 +0330

    Linting

--- components/molecules/carousel/components/prc.js
+++ components/molecules/carousel/components/prc.js
@@ -3,12 +3,12 @@
  */
 
 // React
-import React, { Children, cloneElement } from "react";
-import { string, array } from "prop-types";
+import React, { Children, cloneElement } from 'react'
+import { string, array } from 'prop-types'
 
 // PRC
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from "pure-react-carousel";
-import "pure-react-carousel/dist/react-carousel.es.css";
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel'
+import 'pure-react-carousel/dist/react-carousel.es.css'
 
 export const PRCarousel = ({ children, width, height }) => {
   return (
@@ -17,19 +17,19 @@ export const PRCarousel = ({ children, width, height }) => {
         {Children.map(children, (child, index) => {
           return cloneElement(child, {
             index
-          });
+          })
         })}
       </Slider>
       <ButtonBack>Back</ButtonBack>
       <ButtonNext>Next</ButtonNext>
     </CarouselProvider>
-  );
-};
+  )
+}
 
 PRCarousel.propTypes = {
   children: array,
   width: string,
   height: string
-};
+}
 
-export const PRCarouselSlide = Slide;
+export const PRCarouselSlide = Slide
--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/.jest/fileMock.js"
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/.jest/fileMock.js'
   },
-  modulePaths: ["node_modules", "components", "<rootDir>"],
-  setupFiles: ["<rootDir>/.jest/jest.init.js", "<rootDir>/.jest/register-context.js"],
-  setupFilesAfterEnv: ["<rootDir>/.jest/setupTestAfterEnv.js"],
-  testMatch: ["**/__tests__/*.test.js"],
-  testURL: "http://localhost",
+  modulePaths: ['node_modules', 'components', '<rootDir>'],
+  setupFiles: ['<rootDir>/.jest/jest.init.js', '<rootDir>/.jest/register-context.js'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setupTestAfterEnv.js'],
+  testMatch: ['**/__tests__/*.test.js'],
+  testURL: 'http://localhost',
   transform: {
-    "^.+\\.md?$": "markdown-loader-jest",
-    "^.+\\.js?$": "babel-jest"
+    '^.+\\.md?$': 'markdown-loader-jest',
+    '^.+\\.js?$': 'babel-jest'
   },
-  transformIgnorePatterns: ["/node_modules/(?!(@babel/runtime)/).*/"]
+  transformIgnorePatterns: ['/node_modules/(?!(@babel/runtime)/).*/']
 }
