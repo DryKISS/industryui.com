7550a92cb926363e0a4675522efcdc49d88c6b58
Majid Amiri <majidamiriii@gmail.com>
2019-11-01 17:18:16 +0330

    Added Alert component and useChange

--- components/organisms/login/__tests__/stories.js
+++ components/organisms/login/__tests__/stories.js
@@ -7,12 +7,15 @@ import React, { useState } from 'react'
 
 // UI
 import { Login } from '../'
+import { useChange } from '../../../'
 import Readme from '../README.md'
 import { requestSimulator } from '../../../utils'
+import { withKnobs, select } from '@storybook/addon-knobs'
 
 export default {
   title: 'Organisms/Login',
   component: Login,
+  decorators: [withKnobs],
   parameters: {
     readme: {
       sidebar: Readme
@@ -20,40 +23,40 @@ export default {
   }
 }
 
-export const defaultStory = () => (
-  <Login change={() => {}} email='' submit={() => {}} password='' />
-)
+export const defaultStory = () => {
+  const [change, form] = useChange({ email: '', password: '' })
+  return <Login change={change} email={form.email} submit={() => {}} password={form.password} />
+}
 
 defaultStory.story = {
   name: 'Default'
 }
 
 export const withShowAndHidePassword = () => {
-  const [values, setValues] = useState({
-    email: '',
-    password: ''
-  })
-  const handleChange = e => {
-    const { name, value } = e.target
-    setValues(prev => ({
-      ...prev,
-      [name]: value
-    }))
-  }
+  const [change, form] = useChange({ email: '', password: '' })
   return (
     <Login
-      change={handleChange}
-      email={values.email}
+      change={change}
+      email={form.email}
       submit={() => {}}
-      password={values.password}
+      password={form.password}
       showPassword
     />
   )
 }
 
-export const withBlockSubmitButton = () => (
-  <Login change={() => {}} email='' submit={() => {}} password='' blockSubmitButton />
-)
+export const withBlockSubmitButton = () => {
+  const [change, form] = useChange({ email: '', password: '' })
+  return (
+    <Login
+      change={change}
+      email={form.email}
+      submit={() => {}}
+      password={form.password}
+      blockSubmitButton
+    />
+  )
+}
 
 export const withPlaceholder = () => (
   <Login
@@ -67,33 +70,41 @@ export const withPlaceholder = () => (
 )
 
 export const withHttpRequest = () => {
-  const [values, setValues] = useState({
-    email: '',
-    password: ''
-  })
+  const [change, form] = useChange({ email: '', password: '' })
   const [loading, setLoading] = useState(false)
-  const [result, setResult] = useState('')
-  const handleChange = e => {
-    const { name, value } = e.target
-    setValues(prev => ({
-      ...prev,
-      [name]: value
-    }))
-  }
+  const [result, setResult] = useState({
+    type: '',
+    message: ''
+  })
+  const label = 'Response Type'
+  const options = ['success', 'failure']
+  const defaultValue = 'failure'
+  const groupId = 'GROUP-ID1'
+  const value = select(label, options, defaultValue, groupId)
   const handleSubmit = e => {
     e.preventDefault()
     setLoading(true)
-    requestSimulator('failure')
-      .then(res => setResult(res))
-      .catch(e => setResult(e.message))
+    requestSimulator(value)
+      .then(res =>
+        setResult({
+          type: 'success',
+          message: res
+        })
+      )
+      .catch(e =>
+        setResult({
+          type: 'danger',
+          message: e.message
+        })
+      )
       .finally(() => setLoading(false))
   }
   return (
     <Login
-      change={handleChange}
-      email={values.email}
+      change={change}
+      email={form.email}
       submit={handleSubmit}
-      password={values.password}
+      password={form.password}
       submitResult={result}
       submitLoading={loading}
     />
