36e69f01c92c380a96c7242d84990aaec932ab2b
Kristian Heruc <kristian@heruc.com>
2019-11-08 21:31:07 +0100

    Update useNotifications hook

--- components/services/hooks/useNotifications.js
+++ components/services/hooks/useNotifications.js
@@ -4,13 +4,13 @@
  */
 
 // React
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 // UI
 import { Api, useInterval } from '../../../'
 
 export const useNotifications = (url, params = {}, seconds = 60) => {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState({ messages: [] })
 
   if (!url) {
     return { data }
@@ -18,15 +18,24 @@ export const useNotifications = (url, params = {}, seconds = 60) => {
 
   useInterval(() => {
     const fetchNotifications = async () => {
-      const response = await Api.get(url, params)
+      const res = await Api.get(url, params)
 
-      if (response.status === 200) {
-        setData(response.data)
+      if (res.status === 200 && res.data.messages) {
+        window.localStorage.setItem('active-notifications', JSON.stringify(res.data.messages))
+        setData({ messages: res.data.messages })
       }
     }
 
     fetchNotifications()
   }, seconds * 1000)
+
+  useEffect(() => {
+    // get notifications from localStorage
+    const notifications = JSON.parse(window.localStorage.getItem('active-notifications'))
+    if (notifications) {
+      setData({ messages: notifications })
+    }
+  }, [])
 
   return { data }
 }
