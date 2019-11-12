c927e6053485c6abf222ebb7a296d4ef404a4a74
Kristian Heruc <kristian@heruc.com>
2019-11-12 17:17:02 +0100

    Add padding to Page container

--- components/layouts/page/components/page.js
+++ components/layouts/page/components/page.js
@@ -6,6 +6,9 @@
 // React
 import { node, object, bool } from 'prop-types'
 
+// styled-components
+import styled from 'styled-components'
+
 // UI
 import { Container, MetaHead, PageHeading } from '../../../'
 
@@ -14,16 +17,24 @@ import { Brand, Canonical } from 'config'
 
 export const Page = ({ children, fluid, meta, pageHeading }) => {
   return (
-    <>
+    <StyledPage>
       <MetaHead canonical={Canonical} brand={Brand.name} meta={meta} />
 
       <Container fluid={fluid}>
         {pageHeading && <PageHeading {...pageHeading} />}
         {children}
       </Container>
-    </>
+    </StyledPage>
   )
 }
+
+const StyledPage = styled.div`
+  padding: 30px 0;
+
+  @media (min-width: ${({ theme }) => theme.GRID.breakpoints.lg}px) {
+    padding: 30px 10px;
+  }
+`
 
 Page.propTypes = {
   children: node.isRequired,
