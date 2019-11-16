81d472afd20c62e7ffa5dae649e7d500a830a775
Catalin Besleaga <catalinbesleaga98@gmail.com>
2019-11-16 05:45:50 +0300

    feature: LineChart

--- components/theme/variables/index.js
+++ components/theme/variables/index.js
@@ -12,6 +12,7 @@ import { BARCHART } from '../../molecules/charts/responsiveBar/variables'
 import { FOOTER } from '../../organisms/footer/variables'
 import { GRID } from '../../atoms/grid/variables'
 import { HEADINGS } from '../../atoms/heading/variables'
+import { LINECHART } from '../../molecules/charts/lineChart/variables'
 import { NAVBAR } from '../../molecules/navbar/variables'
 import { PROGRESS, PROGRESSBAR } from '../../atoms/progress/variables'
 import { SIDEBAR } from '../../molecules/sidebar/variables'
@@ -25,6 +26,7 @@ export const Theme = {
   GRID,
   HEADINGS,
   AVATAR,
+  LINECHART,
   NAVBAR,
   PIECHART,
   PROGRESS,
