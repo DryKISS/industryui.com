17d75738c48c659f0425851546b13bc07706eb15
catalin <catalinbesleagadev@gmail.com>
2019-11-18 18:11:09 +0200

    Story: with Dynamic content

--- components/molecules/offCanvas/__tests__/stories.js
+++ components/molecules/offCanvas/__tests__/stories.js
@@ -81,3 +81,77 @@ export const withMailForm = () => {
     </>
   )
 }
+
+export const withDynamicContent = () => {
+  const [isChecked, toggleCheck] = useState(true)
+  const [previewIndex, changePreview] = useState(1)
+
+  const previews = {
+    1: {
+      headerText: 'Homyze First',
+      component: function () {
+        return (
+          <>
+            <Form submit={() => {}}>
+              <Input
+                change={() => {}}
+                label='Email'
+                id='email'
+                placeholder='Enter email address'
+                type='email'
+              />
+              <Button secondary type='submit'>
+                Send reset link
+              </Button>
+            </Form>
+            <br />
+            <Progress size='md'>
+              <ProgressBar striped animated now={90}>
+                90%
+              </ProgressBar>
+            </Progress>
+          </>
+        )
+      }
+    },
+    2: {
+      headerText: 'Homyze Second',
+      component: function () {
+        return <div>2</div>
+      }
+    },
+    3: {
+      headerText: 'Homyze Third',
+      component: function () {
+        return <div>3</div>
+      }
+    }
+  }
+
+  return (
+    <>
+      <label htmlFor='offCanvas'>Show/Hide OffCanvas </label>
+      <input
+        onChange={e => toggleCheck(!isChecked)}
+        id='offCanvas'
+        type='checkbox'
+        checked={isChecked}
+      />
+      <br />
+      <Button onClick={e => changePreview(1)}>Preview one</Button>
+      <br />
+      <br />
+      <Button onClick={e => changePreview(2)}>Preview two</Button>
+      <br />
+      <br />
+      <Button onClick={e => changePreview(3)}>Preview three</Button>
+      <OffCanvas
+        headerText={previews[previewIndex].headerText}
+        show={isChecked}
+        toggleShow={toggleCheck}
+      >
+        <ContainerDiv>{previews[previewIndex].component()}</ContainerDiv>
+      </OffCanvas>
+    </>
+  )
+}
