04c3a3ab5ea07e0c9a3923a6831285a2b71a3734
catalin <catalinbesleagadev@gmail.com>
2019-11-18 17:41:02 +0200

    Dynamic content

--- components/molecules/offCanvas/__tests__/stories.js
+++ components/molecules/offCanvas/__tests__/stories.js
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import { withKnobs, text } from '@storybook/addon-knobs'
+import styled from 'styled-components'
 
 // UI
 import { OffCanvas } from '../'
 import Readme from '../README.md'
 
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { Button, Input, Form, Progress, ProgressBar } from '../../../'
 
 export default {
   title: 'Molecules/OffCanvas',
@@ -32,6 +34,49 @@ export const main = () => {
       />
       <OffCanvas headerText={textKnob} show={isChecked} toggleShow={toggleCheck}>
         Components go here
+      </OffCanvas>
+    </>
+  )
+}
+
+const ContainerDiv = styled.div`
+  padding: 10px;
+`
+
+export const withMailForm = () => {
+  const [isChecked, toggleCheck] = useState(true)
+  const textKnob = text('headerText', 'Homyze')
+
+  return (
+    <>
+      <label htmlFor='offCanvas'>Show/Hide OffCanvas </label>
+      <input
+        onChange={e => toggleCheck(!isChecked)}
+        id='offCanvas'
+        type='checkbox'
+        checked={isChecked}
+      />
+      <OffCanvas headerText={textKnob} show={isChecked} toggleShow={toggleCheck}>
+        <ContainerDiv>
+          <Form submit={() => {}}>
+            <Input
+              change={() => {}}
+              label='Email'
+              id='email'
+              placeholder='Enter email address'
+              type='email'
+            />
+            <Button secondary type='submit'>
+              Send reset link
+            </Button>
+          </Form>
+          <br />
+          <Progress size='md'>
+            <ProgressBar striped animated now={90}>
+              90%
+            </ProgressBar>
+          </Progress>
+        </ContainerDiv>
       </OffCanvas>
     </>
   )
