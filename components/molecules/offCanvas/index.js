304113fbf4fd681303e99ba2ea0155367bbf626d
Catalin <catalin4ic@gmail.com>
2019-11-19 11:43:14 +0200

    animate only transform; position fixed; zIndex 9999 (#104)

--- components/molecules/offCanvas/index.js
+++ components/molecules/offCanvas/index.js
@@ -7,15 +7,16 @@ import { Close } from '../../'
 
 const OffCanvasDiv = styled.div`
   box-shadow: ${({ show }) => (show ? '-5px 5px 22px -15px rgba(0,0,0,0.75)' : 'none')};
-  position: absolute;
+  position: fixed;
   top: 0;
   right: 0;
   width: ${({ width }) => width};
   height: 100vh;
   background-color: #fff;
   transform: ${({ width, show }) => `translateX(${show ? 0 : width})`};
-  transition: 375ms cubic-bezier(0.6, -0.28, 0.735, 0.045);
+  transition: transform 375ms cubic-bezier(0.6, -0.28, 0.735, 0.045);
   will-change: transform;
+  z-index: 9999
 `
 
 const OffCanvasHeader = styled.div`
