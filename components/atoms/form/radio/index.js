d3a3a70be80aa5662c16707908c380dd13ee67fd
Ian Warner <iwarner@users.noreply.github.com>
2019-11-19 22:38:10 +0100

    Iw properties (#106)
    * fix: Postiion for canvas
    
    * fix: standards for radio comp, updated style with CSS
    
    * chore: lint fix on offcanvas

--- components/atoms/form/radio/index.js
+++ components/atoms/form/radio/index.js
@@ -6,7 +6,7 @@
 import { array, bool, func, string } from 'prop-types'
 
 // Style
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Radio = ({ change, data, legend, stacked }) => {
   const renderRadio = () => {
@@ -39,6 +39,25 @@ export const Radio = ({ change, data, legend, stacked }) => {
   )
 }
 
+const StyledLabel = styled.label`
+  margin-right: 1.25rem;
+  padding-left: 1.25rem;
+  position: relative;
+  ${({ stacked }) =>
+    stacked &&
+    css`
+      display: block;
+      margin: 0.5rem 0;
+      padding-left: 1.25rem;
+    `}
+`
+
+const StyledInput = styled.input`
+  margin-top: 0.05rem;
+  margin-left: -1.25rem;
+  position: absolute;
+`
+
 Radio.propTypes = {
   change: func.isRequired,
   data: array.isRequired,
@@ -50,17 +69,3 @@ Radio.defaultProps = {
   legend: '',
   stacked: false
 }
-
-const StyledLabel = styled.label`
-  ${props => props.stacked && 'display: block;'}
-  padding-left: 1.25rem;
-  ${props => (props.stacked ? 'margin: .5rem 0;' : 'margin-right: 1.25rem;')}
-  ${props => props.stacked && 'padding-left: 1.25rem;'}
-  position: relative;
-`
-
-const StyledInput = styled.input`
-  margin-top: 0.05rem;
-  margin-left: -1.25rem;
-  position: absolute;
-`
