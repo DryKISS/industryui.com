3a8c024d1163d1f2aa61c12fd57c357fdabf564d
Kristian Heruc <kristian@heruc.com>
2019-11-23 13:20:45 +0100

    Update Stepper timings styles

--- components/molecules/stepper/components/timings.js
+++ components/molecules/stepper/components/timings.js
@@ -11,27 +11,32 @@ import styled from 'styled-components'
 export const StepperTimings = ({ className, timings }) => {
   return (
     <StyledStepperTimings>
-      <li>
-        <StyledLabel>Start time: </StyledLabel>
-        <StyledValue>{timings.start}</StyledValue>
-      </li>
-      <li>
-        <StyledLabel>Scheduled end time: </StyledLabel>
-        <StyledValue>{timings.scheduled}</StyledValue>
-      </li>
-      <li>
-        <StyledLabel>Actual end time: </StyledLabel>
-        <StyledValue success>{timings.actual}</StyledValue>
-      </li>
+      <ul>
+        <li>
+          <StyledLabel>Start time: </StyledLabel>
+          <StyledValue>{timings.start}</StyledValue>
+        </li>
+        <li>
+          <StyledLabel>Scheduled end time: </StyledLabel>
+          <StyledValue>{timings.scheduled}</StyledValue>
+        </li>
+        <li>
+          <StyledLabel>Actual end time: </StyledLabel>
+          <StyledValue success>{timings.actual}</StyledValue>
+        </li>
+      </ul>
     </StyledStepperTimings>
   )
 }
 
-const StyledStepperTimings = styled.ul`
+const StyledStepperTimings = styled.div`
   display: inline-block;
-  list-style: none;
-  margin: 10px 0;
-  padding: 0;
+
+  > ul {
+    list-style: none;
+    margin: 10px 0 20px 20px;
+    padding: 0;
+  }
 `
 
 const StyledLabel = styled.span`
