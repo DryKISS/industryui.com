11fee64fb3e961649a237816d0318d13ab1f5e43
Catalin <catalinbesleagadev@gmail.com>
2019-11-23 21:39:27 +0200

    Atoms/Form/ReactSelect (#111)

--- components/atoms/form/reactSelect/__tests__/stories.js
+++ components/atoms/form/reactSelect/__tests__/stories.js
@@ -3,19 +3,31 @@
  */
 
 // React
-import React from 'react'
+import React, { useState } from 'react'
 
 // Styled Components
 import styled from 'styled-components'
+
+// Storybook
+import { boolean, text, withKnobs } from '@storybook/addon-knobs'
+
 // UI
-import { ReactSelect } from '../'
+import { Button, Input, Form, ReactSelect, useChange } from 'components/atoms'
 import Readme from '../README.md'
-import { useChange } from '../../../../'
 
 import { OPTIONS } from '../__mocks__/options'
+
+const Wrapper = styled.div`
+  width: 400px;
+  margin-left: 40px;
+`
+
+const LayoutDecorator = storyFn => <Wrapper>{storyFn()}</Wrapper>
+
 export default {
   title: 'Atoms/Form/ReactSelect',
   component: ReactSelect,
+  decorators: [LayoutDecorator, withKnobs],
   parameters: {
     readme: {
       sidebar: Readme
@@ -23,47 +35,97 @@ export default {
   }
 }
 
-const Wrapper = styled.div`
-  width: 400px;
-`
+const useKnobs = (props = {}) => {
+  const { defaultProps } = ReactSelect
+  const knobs = Object.keys(defaultProps)
+    .filter(key => typeof defaultProps[key] === 'string' || typeof defaultProps[key] === 'boolean')
+    .reduce((acc, key) => {
+      const value = props[key] !== undefined ? props[key] : defaultProps[key]
+      const knob = typeof value === 'boolean' ? boolean(key, value) : text(key, value)
+      acc[key] = knob
+      return acc
+    }, {})
+
+  return knobs
+}
 
 export const Single = () => {
   const [change] = useChange({ select: {} })
+  const knobs = useKnobs()
   return (
-    <Wrapper>
-      <ReactSelect label='React Select' options={OPTIONS} isClearable id='select' change={change} />
-    </Wrapper>
+    <ReactSelect
+      {...knobs}
+      change={change}
+      id='select'
+      isClearable
+      label='React Select'
+      options={OPTIONS}
+    />
   )
 }
 
 export const Multi = () => {
   const [change] = useChange({ select: [] })
+  const knobs = useKnobs({ isMulti: true })
   return (
-    <Wrapper>
-      <ReactSelect
-        label='React Select'
-        isMulti
-        options={OPTIONS}
-        isClearable
-        id='select'
-        change={change}
-      />
-    </Wrapper>
+    <ReactSelect
+      change={change}
+      id='select'
+      isClearable
+      label='React Select'
+      options={OPTIONS}
+      {...knobs}
+    />
   )
 }
 
 export const withDefaultValue = () => {
   const [change] = useChange({ select: {} })
+  const knobs = useKnobs()
   return (
-    <Wrapper>
+    <ReactSelect
+      change={change}
+      defaultValue={OPTIONS[0]}
+      id='select'
+      isClearable
+      label='React Select'
+      options={OPTIONS}
+      {...knobs}
+    />
+  )
+}
+
+export const insideAForm = () => {
+  const placeholder = 'Name submitted: '
+  const [name, setName] = useState(placeholder)
+  const [change] = useChange({ select: {} })
+  const knobs = useKnobs()
+
+  const handleFormSubmit = ({
+    target: {
+      name: { value }
+    }
+  }) => {
+    setName(placeholder + value)
+  }
+  return (
+    <Form submit={handleFormSubmit}>
+      {name}
+      <br />
+      <br />
+      <Input change={() => {}} label='Name' id='name' placeholder='Enter Your Name' type='text' />
       <ReactSelect
-        label='React Select'
-        options={OPTIONS}
-        isClearable
-        id='select'
         change={change}
         defaultValue={OPTIONS[0]}
+        id='select'
+        isClearable
+        label='React Select'
+        options={OPTIONS}
+        {...knobs}
       />
-    </Wrapper>
+      <Button secondary type='submit'>
+        Submit
+      </Button>
+    </Form>
   )
 }
