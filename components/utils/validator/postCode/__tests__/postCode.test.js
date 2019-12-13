4c66e26da4a27a233873be82ef9dedd5008765b4
iwarner <ian.warner@drykiss.com>
2019-12-13 10:47:41 +0100

    fix: added a story for the Utils we want this same pattern for all

---
+++ components/utils/validator/postCode/__tests__/postCode.test.js
@@ -0,0 +1,10 @@
+/**
+ * Utils - PostCode
+ */
+import { main } from './stories'
+import renderer from 'react-test-renderer'
+
+it('renders correctly', () => {
+  const tree = renderer.create(main()).toJSON()
+  expect(tree).toMatchSnapshot()
+})
