902e81a918c5a4b68ca9a614478208e6c1a1bcab
iwarner <ian.warner@drykiss.com>
2020-01-29 11:29:23 +0330

    Added RadioField to new form components

---
+++ components/form/radio/__mocks__/radio.js
@@ -0,0 +1,17 @@
+export const RADIO_GENDER = (genderToggle = 'no') => [
+  {
+    id: 'no',
+    label: 'No preference',
+    value: 'no'
+  },
+  {
+    id: 'male',
+    label: 'Male',
+    value: 'male'
+  },
+  {
+    id: 'female',
+    label: 'Female',
+    value: 'female'
+  }
+]
