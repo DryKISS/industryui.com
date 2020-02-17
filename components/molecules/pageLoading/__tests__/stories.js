1f5641e6cd361e7282fd4c21ba64c925314e4b43
Majid Amiri <majidamiriii@gmail.com>
2020-02-18 02:45:44 +0330

    Add some stories for PageLoading

---
+++ components/molecules/pageLoading/__tests__/stories.js
@@ -0,0 +1,22 @@
+/**
+ * Page Heading
+ */
+
+// React
+import React from 'react'
+
+// Storybook
+import { Wrapper } from 'decorators'
+
+// UI
+import { PageLoading, LdsSpinner } from '../'
+
+export default {
+  title: 'Molecules/PageLoading',
+  component: PageLoading,
+  decorators: [Wrapper]
+}
+
+export const main = () => <PageLoading indicator={<LdsSpinner />} />
+
+export const withChildren = () => <PageLoading>Loading...</PageLoading>
