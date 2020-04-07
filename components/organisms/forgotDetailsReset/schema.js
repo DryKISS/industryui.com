9c6bc0e3ea49bb629617d1a350603f5e79c22437
Ian Warner <iwarner@users.noreply.github.com>
2020-04-07 15:56:42 +0200

    chore: fixed linting (#238)
    * chore: fixed linting
    
    * chore: added yup

---
+++ components/organisms/forgotDetailsReset/schema.js
@@ -0,0 +1,17 @@
+/**
+ * Organisms - Forgot Details Reset - Schema
+ */
+
+// Yup
+import * as yup from 'yup'
+
+export const ForgotDetailsResetSchema = yup.object().shape({
+  password: yup
+    .string()
+    .required('Please Enter a password')
+    .min(8, 'Password is too short - should be 8 chars minimum.')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
+      'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character'
+    )
+})
---
+++ components/utils/validator/uuid4.js
@@ -0,0 +1,8 @@
+/**
+ * Validator: UUID4
+ */
+export const validatorUuid4 = uuid => {
+  const regExp = /^[a-z0-9-]{36}$/gi
+
+  return regExp.test(uuid)
+}
