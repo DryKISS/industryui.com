4328e80cd341e28f1ae8edbe456402dbe1d8ee5f
Majid <58373846+majiiid-drykiss@users.noreply.github.com>
2020-04-09 13:53:14 +0430

    Add Avatar Action and other improvements (#241)
    * Add Avatar Action and other improvements
    
    * Add Image and its stories
    
    Co-authored-by: Majid Amiri <majidamiriii@gmail.com>

--- components/atoms/avatar/__tests__/stories.js
+++ components/atoms/avatar/__tests__/stories.js
@@ -24,4 +24,26 @@ export default {
   }
 }
 
-export const main = () => <Avatar content='Avatar' context={Context()} size={number('Size', 60)} />
+const BaseComponent = props => (
+  <Avatar content='Avatar' context={Context()} size={number('Size', 60)} {...props} />
+)
+
+export const main = () => <BaseComponent />
+
+export const withAction = () => (
+  <BaseComponent action='Edit' actionClick={() => window.alert('Clicked')} />
+)
+
+export const withImage = () => (
+  <BaseComponent src='https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg' />
+)
+
+export const withGravatar = () => <BaseComponent gmail='test@gmail.com' />
+
+export const withImageAndAction = () => (
+  <BaseComponent
+    action='Edit'
+    actionClick={() => window.alert('Clicked')}
+    src='https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg'
+  />
+)
