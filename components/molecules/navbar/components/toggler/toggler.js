b79320ede27a3e0328657dabff5203e913f5ee0e
zemqi <zemqee@gmail.com>
2020-04-28 09:25:52 +0600

    fix: changed toggler

--- components/molecules/navbar/components/toggler/toggler.js
+++ components/molecules/navbar/components/toggler/toggler.js
@@ -1,5 +1,5 @@
 /**
- * Navbar — Toggler
+ * Navbar - Toggler
  */
 
 // React
@@ -9,20 +9,13 @@ import { bool, func } from 'prop-types'
 import { Icon, MEDIA_QUERY } from '../../../../'
 
 // Style
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 
 export const Toggler = ({ custom, handleMenuClick, visible }) => {
-  const CustomTogglerIconClosed = () => (
-    <StyledIcon>
-      <StyledlineTopAnimation />
-      <StyledlineBottomAnimation />
-    </StyledIcon>
-  )
-
   const CustomTogglerIconOpen = () => (
     <StyledIcon>
-      <StyledlineTop />
-      <StyledlineBottom />
+      <div />
+      <div />
     </StyledIcon>
   )
 
@@ -32,7 +25,7 @@ export const Toggler = ({ custom, handleMenuClick, visible }) => {
       aria-label='Toggle navigation'
       onClick={handleMenuClick}
     >
-      {!visible && <CustomTogglerIconClosed />}
+      {!visible && <CustomTogglerIconOpen />}
 
       {visible && <CustomTogglerIconOpen />}
     </StyledToggler>
@@ -60,42 +53,22 @@ export const Toggler = ({ custom, handleMenuClick, visible }) => {
 
 const StyledIcon = styled.div`
   margin-top: 0.25rem;
-`
-const ToggleAnimationTop = keyframes`
-  from {
-    transform: rotate(1deg);
+  div {
+    width: 2rem;
+    height: 0.125rem;
+    background: black;
+    margin-bottom: 0.375rem;
+    transition: .3s all linear;
   }
-  to {
-    transform: rotate(-20deg);
+  div:nth-child(1) {
+    &:active {
+      transform: rotate(45deg);
+    }
   }
-`
-const ToggleAnimationBottom = keyframes`
-  from {
-    transform: rotate(1deg);
-  }
-  to {
-    transform: rotate(20deg);
-  }
-`
-const StyledlineTop = styled.div`
-  width: 2rem;
-  height: 0.125rem;
-  background: black;
-  margin-bottom: 0.375rem;
-`
-const StyledlineBottom = styled.div`
-  width: 2rem;
-  height: 0.125rem;
-  background: black;
-`
-const StyledlineTopAnimation = styled(StyledlineTop)`
-  &:active {
-    animation: 0.2s ${ToggleAnimationTop} ease-out;
-  }
-`
-const StyledlineBottomAnimation = styled(StyledlineTop)`
-  &:active {
-    animation: 0.2s ${ToggleAnimationBottom} ease-out;
+  div:nth-child(2) {
+    &:active {
+      transform: rotate(-45deg);
+    }
   }
 `
 
@@ -107,7 +80,6 @@ const StyledToggler = styled.a`
     theme.NAVBAR.fontSizeToggler ? theme.NAVBAR.fontSizeToggler : '0.8125rem'};
   padding: ${({ theme }) =>
     theme.NAVBAR.paddingToggler ? theme.NAVBAR.paddingToggler : '1.25rem 0 1.25rem 1rem'};
-
   &:hover {
     color: ${({ theme }) =>
       theme.NAVBAR.colourHoverToggler ? theme.NAVBAR.colourHoverToggler : theme.COLOUR.primary};
