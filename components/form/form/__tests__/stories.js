000e0b8152cd86f8ec8f0ab975b69961baaa484b
Majid Amiri <majid@drykiss.com>
2020-06-23 00:44:25 +0430

    Add form elements to all elements story

--- components/form/form/__tests__/stories.js
+++ components/form/form/__tests__/stories.js
@@ -10,7 +10,7 @@ import { text, withKnobs } from '@storybook/addon-knobs'
 import { Wrapper } from 'decorators'
 
 // React Hook Form
-import { useForm } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers'
 
@@ -20,18 +20,30 @@ import {
   CheckboxField,
   Column,
   CurrencyInput,
+  DatePickerCalendar,
   Divider,
+  Dropzone,
   FormError,
   FormField,
   FormForm,
   FormLabel,
   Heading,
+  RadioField,
+  ReactSelectField,
   Row,
+  SelectField,
   Space,
-  Text
+  Text,
+  TextareaField
 } from 'components'
 
 import Readme from '../README.md'
+
+// Data
+import enGB from 'date-fns/locale/en-GB'
+import { RADIO_GENDER } from '../../radio/__mocks__/radio'
+import { Options } from '../../reactSelect/__mocks__/options'
+import { COLOURS } from '../../select/__mocks__/colours'
 
 export default {
   title: 'Form',
@@ -68,7 +80,7 @@ const checkbox = [
 ]
 
 export const all = () => {
-  const { errors, handleSubmit, register } = useForm({ resolver: yupResolver(schema) })
+  const { control, errors, handleSubmit, register } = useForm({ resolver: yupResolver(schema) })
   const onSubmit = data => {}
 
   console.log(errors)
@@ -100,7 +112,7 @@ export const all = () => {
         </Column>
 
         <Column md={6}>
-          <FormLabel label='Email;'>
+          <FormLabel label='Email'>
             <FormField {...defaultProps} name='email' placeholder='Enter Email' />
             <Text size='sm'>We'll never share your email with anyone else.</Text>
             {errors.email && <FormError message={errors.email.message} />}
@@ -125,6 +137,38 @@ export const all = () => {
             {errors.readonly && <FormError message={errors.readonly.message} />}
           </FormLabel>
         </Column>
+
+        <Column md={6}>
+          <FormLabel label='Datepicker'>
+            <DatePickerCalendar
+              {...defaultProps}
+              control={control}
+              locale={enGB}
+              name='datePicker'
+            />
+          </FormLabel>
+        </Column>
+
+        <Column md={6}>
+          <FormLabel label='React Select'>
+            <ReactSelectField
+              {...defaultProps}
+              control={control}
+              name='reactSelecgt'
+              options={Options}
+            />
+          </FormLabel>
+        </Column>
+
+        <Column md={12}>
+          <FormLabel children='' label='Dropzone' />
+          <Controller
+            {...defaultProps}
+            render={() => <Dropzone multiple={false} />}
+            control={control}
+            name='dropzone'
+          />
+        </Column>
       </Row>
 
       <Divider size='md' />
@@ -135,12 +179,22 @@ export const all = () => {
         <FormField {...defaultProps} name='input' />
       </FormLabel>
 
+      <FormLabel label='Textarea'>
+        <TextareaField {...defaultProps} rows={2} name='textArea' />
+      </FormLabel>
+
+      <FormLabel label='Select'>
+        <SelectField {...defaultProps} options={COLOURS} name='select' />
+      </FormLabel>
+
       <CheckboxField
         {...defaultProps}
         data={checkbox}
         legend={text('Legend', 'Checkbox Legend')}
         name='checkbox'
       />
+
+      <RadioField {...defaultProps} data={RADIO_GENDER()} legend='Gender?' name='radio' />
 
       <Button content='Submit' type='submit' />
     </FormForm>
