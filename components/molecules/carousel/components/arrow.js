c8f3cbe6e5f3f9f6ae5dde3918c929e8be5cfafb
Majid Amiri <majid@drykiss.com>
2020-07-14 02:23:15 +0430

    Add overlay background to Arrow

--- components/molecules/carousel/components/arrow.js
+++ components/molecules/carousel/components/arrow.js
@@ -4,51 +4,63 @@
 
 // React
 import React from 'react'
-import { func, string } from 'prop-types'
+import { func, oneOf, string } from 'prop-types'
 
 // Style
 import styled, { css } from 'styled-components'
 import { Icon } from '../../../'
 
 export const CarouselArrow = ({ clickFunction, context, direction, icon, position }) => (
-  <StyledArrow
-    context={context}
-    direction={direction}
-    icon={icon}
-    onClick={clickFunction}
-    position={position}
-  />
+  <StyledContainer direction={direction} onClick={clickFunction}>
+    <StyledArrow context={context} icon={icon} position={position} />
+  </StyledContainer>
 )
+
+const StyledContainer = styled.div`
+  position: absolute;
+  top: 0;
+  ${({ direction }) => (direction === 'left' ? 'left: 0;' : 'right: 0;')}
+  background: linear-gradient(${({ direction }) =>
+    direction === 'left' ? 90 : 270}deg, rgba(0, 0, 0, 0.1) 0%, rgba(255, 255, 255, 0) 100%);
+  height: 100%;
+  width: 80px;
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
+  z-index: 1;
+  padding: 1rem 0;
+
+  &:hover {
+    background: linear-gradient(${({ direction }) =>
+      direction === 'left' ? 90 : 270}deg, rgba(0, 0, 0, 0.2) 0%, rgba(255, 255, 255, 0) 100%);
+  }
+`
 
 const StyledArrow = styled(Icon)`
   color: ${({ context, theme: { COLOUR } }) => COLOUR[context]};
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  ${({ direction }) => (direction === 'left' ? 'left: 1rem;' : 'right: 1rem;')}
-
-  && {
-    font-size: 2rem;
-  }
+  align-self: center;
 
   ${({ position }) =>
     position === 'top' &&
     css`
-      top: 10%;
+      align-self: flex-start;
     `};
 
   ${({ position }) =>
     position === 'bottom' &&
     css`
-      top: unset;
-      bottom: 10%;
+      align-self: flex-end;
     `};
+
+  && {
+    font-size: 2rem;
+  }
 `
 
 CarouselArrow.propTypes = {
   clickFunction: func.isRequired,
   context: string.isRequired,
-  direction: string.isRequired,
+  direction: oneOf(['left', 'right']).isRequired,
   icon: string.isRequired,
   position: string.isRequired
 }
