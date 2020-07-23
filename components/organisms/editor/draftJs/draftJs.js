afc1aa1bc32b363b590baa93fc08b29046d4ccb6
Elio Suarez <Esuarez.gcc@gmail.com>
2020-07-23 15:22:02 +0100

    fixing the value to be modified

--- components/organisms/editor/draftJs/draftJs.js
+++ components/organisms/editor/draftJs/draftJs.js
@@ -18,10 +18,14 @@ export const DraftJs = () => {
     setEditorState(RichUtils.toggleInlineStyle(editorState, style))
   }
 
+  // TODO: Do the inputList dynamic creating a map with the Key-Value to generate the needed
   return (
     <>
-      <input type='button' value='BOLD' onMouseDown={toggleInlineStyle} />
-      <input type='button' value='ITALIC' onMouseDown={toggleInlineStyle} />
+      <input type='button' value='B' data-style='BOLD' onMouseDown={toggleInlineStyle} />
+      <input type='button' value='I' data-style='ITALIC' onMouseDown={toggleInlineStyle} />
+      <input type='button' value='S' data-style='STRIKETHROUGH' onMouseDown={toggleInlineStyle} />
+      <input type='button' value='U' data-style='UNDERLINE' onMouseDown={toggleInlineStyle} />
+
       <Editor editorState={editorState} onChange={setEditorState} />
     </>
   )
