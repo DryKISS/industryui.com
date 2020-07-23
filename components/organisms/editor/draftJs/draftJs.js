982b1ea2c2a2dda21884156852b6556aac346d5e
Elio Suarez <Esuarez.gcc@gmail.com>
2020-07-23 16:45:57 +0100

    working example without giving proper block styles

--- components/organisms/editor/draftJs/draftJs.js
+++ components/organisms/editor/draftJs/draftJs.js
@@ -7,13 +7,17 @@ import React, { useState } from 'react'
 
 // Draft JS
 import { Editor, EditorState, RichUtils } from 'draft-js'
+import 'draft-js/dist/Draft.css'
+import { BlockType } from './constants'
+
+// TODO: Check how we name our services/DataType
 
 export const DraftJs = () => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty())
 
   const toggleInlineStyle = event => {
     event.preventDefault()
-    const style = event.currentTarget.getAttribute('value')
+    const style = event.currentTarget.getAttribute('data-style')
     console.log('The style ', style)
     setEditorState(RichUtils.toggleInlineStyle(editorState, style))
   }
@@ -26,6 +30,7 @@ export const DraftJs = () => {
     }
     return 'not-handled'
   }
+
   // TODO: Do the inputList dynamic creating a map with the Key-Value to generate the needed
   return (
     <>
@@ -33,12 +38,20 @@ export const DraftJs = () => {
       <input type='button' value='I' data-style='ITALIC' onMouseDown={toggleInlineStyle} />
       <input type='button' value='S' data-style='STRIKETHROUGH' onMouseDown={toggleInlineStyle} />
       <input type='button' value='U' data-style='UNDERLINE' onMouseDown={toggleInlineStyle} />
-
       <Editor
         editorState={editorState}
+        blockStyleFn={myBlockStyleFn}
         handleKeyCommand={handleKeyCommand}
         onChange={setEditorState}
       />
     </>
   )
+}
+
+function myBlockStyleFn (contentBlock) {
+  const type = contentBlock.getType()
+  if (type === BlockType.BLOCKQUOTE) {
+    // TODO: return the right css according to be applied into the whole block
+    return undefined
+  }
 }
