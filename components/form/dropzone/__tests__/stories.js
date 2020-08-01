ad814902b87f373d4b0cd440132c2f5a9bbeb58d
Majid Amiri <majid@drykiss.com>
2020-08-01 17:49:59 +0430

    feat: add Dropzone to Form components

---
+++ components/form/dropzone/__tests__/stories.js
@@ -0,0 +1,60 @@
+/**
+ * React Select
+ */
+
+// React
+import React from 'react'
+
+// Storybook
+import { boolean, withKnobs } from '@storybook/addon-knobs'
+import { Wrapper } from 'decorators'
+
+// useForm
+import { useForm } from 'react-hook-form'
+
+// UI
+import { Button, FormForm, FormLabel, DropzoneField } from 'components'
+import Readme from '../README.md'
+
+export default {
+  title: 'Form/Dropzone',
+  component: DropzoneField,
+  decorators: [Wrapper, withKnobs],
+  parameters: {
+    readme: {
+      sidebar: Readme
+    }
+  }
+}
+
+const BaseComponent = (props = {}) => {
+  const { control, errors, handleSubmit } = useForm()
+  const onSubmit = data => {
+    console.log('data: ', data)
+  }
+
+  const defaultProps = {
+    accept: '*/image',
+    control,
+    disabled: boolean('Disabled', false),
+    errors: errors,
+    multiple: boolean('Multiple', false),
+    ...props
+  }
+
+  return (
+    <FormForm handleSubmit={handleSubmit(onSubmit)}>
+      <FormLabel label='Dropzone'>
+        <DropzoneField {...defaultProps} />
+      </FormLabel>
+
+      {errors.dropzone && <p>{errors.dropzone.message}</p>}
+
+      <Button content='Submit' secondary type='submit' />
+    </FormForm>
+  )
+}
+
+export const Single = () => {
+  return <BaseComponent />
+}
