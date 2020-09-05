6be9cd813da4f03e45a5a8a0ae42565b5a9137bb
mohsen parvar <mhsnprvrsec@gmail.com>
2020-09-05 17:38:35 +0430

    feat(ImageLocation):minor css changes in marker

--- components/molecules/imageLocation/components/imageMarker.js
+++ components/molecules/imageLocation/components/imageMarker.js
@@ -32,26 +32,23 @@ const blinker = keyframes`
     50% {
     opacity: 0;
   }`
-const transition = css`
-  transition-duration: 0.2s;
-  transition-property: left, top;
-`
-const currection = css`
+
+const commonMarkerCss = css`
+  animation: ${({ styles }) =>
+    styles?.animation === 'blinker'
+      ? css`
+          ${blinker} 1s ease-in-out infinite forwards
+        `
+      : ''};
   margin-left: ${({ styles }) =>
     '-' + (styles?.width ? Number(styles.width.replace('px', '') / 2) + 'px' : '7px')};
   margin-top: ${({ styles }) =>
     '-' + (styles?.height ? Number(styles.height.replace('px', '') / 2) + 'px' : '7px')};
-`
-const StyledIcon = styled(Icon)`
-  color: ${({ styles }) => (styles?.color ? styles?.color : 'red')};
+  pointer-events: none;
   position: absolute;
-  background-color: white;
-  animation: ${({ styles }) =>
-    styles?.animation === 'blinker'
-      ? css`
-          ${blinker} 1s ease-in-out infinite forwards
-        `
-      : ''};
+  transition-duration: 0.2s;
+  transition-property: left, top;
+
   ${({ coordinates }) =>
     coordinates &&
     css`
@@ -59,33 +56,21 @@ const StyledIcon = styled(Icon)`
       left: ${coordinates.x}%;
       top: ${coordinates.y}%;
     `}
-	${currection}
-  ${transition}
+`
+const StyledIcon = styled(Icon)`
+  background-color: white;
+  color: ${({ styles }) => (styles?.color ? styles?.color : 'red')};
+
+  ${commonMarkerCss}
 `
 
 const StyledMarker = styled.div`
-  animation: ${({ styles }) =>
-    styles?.animation === 'blinker'
-      ? css`
-          ${blinker} 1s ease-in-out infinite forwards
-        `
-      : ''};
   background-color: ${({ styles }) => (styles?.color ? styles.color : 'red')};
   border-radius: ${({ styles }) => (styles?.borderRadius ? styles.borderRadius : '50%')};
   display: none;
-  ${({ coordinates }) =>
-    coordinates &&
-    css`
-      display: block;
-      left: ${coordinates.x}%;
-      top: ${coordinates.y}%;
-    `}
-
   height: ${({ styles }) => (styles?.height ? styles.height : '15px')};
-  position: absolute;
   width: ${({ styles }) => (styles?.width ? styles.width : '15px')};
-	${currection}
-  ${transition}
+  ${commonMarkerCss}
 `
 
 ImageMarker.propTypes = {
