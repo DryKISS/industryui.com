07562e98988ec3cd0c43a9e888a16ddba2a3082f
mohsen parvar <mhsnprvrsec@gmail.com>
2020-09-21 11:01:01 +0330

    feat(useComponentComunication):added story

--- components/hooks/useComponentComunication/useSubscription.js
+++ components/hooks/useComponentComunication/useSubscription.js
@@ -1,14 +1,17 @@
 import { useEffect } from 'react'
 import { array, func, instanceOf, string } from 'prop-types'
 import { Subject } from 'rxjs'
+
 const useComponentComunication = props => {
   const { onRecieve, dependencies, id, messageName, subscriber } = props
+
   useEffect(() => {
     const Subscription = subscriber.subscribe(message => {
       if (message.name === messageName && (!id || id === message.id)) {
         onRecieve(message.payload)
       }
     })
+
     return () => {
       Subscription.unsubscribe()
     }
