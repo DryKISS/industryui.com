de94916e5d977facb3af0b94985762135b1e2728
mohsen parvar <mhsnprvrsec@gmail.com>
2020-09-23 19:08:21 +0330

    fix(Text):synced sizes with designs

--- components/atoms/text/variables.js
+++ components/atoms/text/variables.js
@@ -10,21 +10,23 @@ export const TEXT_STYLE = {
     xxxl: 'JetBrains Mono ExtraBold'
   },
   fontSize: {
+    xxs: '0.625rem',
     xs: '0.75rem',
     sm: '0.875rem',
     md: '1rem',
     lg: '1.25rem',
     xl: '1.5rem',
-    xxl: '1.25rem',
-    xxxl: '2.5rem'
+    xxl: '2rem',
+    xxxl: '3rem'
   },
   lineHeight: {
-    xs: '1.333',
-    sm: '1.429',
-    md: '1.5',
-    lg: '1.4',
-    xl: '1.333',
-    xxl: '1.25',
-    xxxl: '1.25rem'
+    xxs: '1rem',
+    xs: '1.125rem',
+    sm: '1.375rem',
+    md: '1.5rem',
+    lg: '1.75rem',
+    xl: '2.25rem',
+    xxl: '3rem',
+    xxxl: '4.5rem'
   }
 }
