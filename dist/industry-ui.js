8abad67b03489475d263c2e0489987b122ff6d5a
iwarner <ian.warner@drykiss.com>
2020-09-24 18:11:31 +0200

    fix(components): minimal build

--- dist/industry-ui.js
+++ dist/industry-ui.js
@@ -457,11 +457,11 @@ var getUrlParameter = function getUrlParameter(name) {
 
 function createCommonjsModule(fn, basedir, module) {
 	return module = {
-	  path: basedir,
-	  exports: {},
-	  require: function (path, base) {
-      return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
-    }
+		path: basedir,
+		exports: {},
+		require: function (path, base) {
+			return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
+		}
 	}, fn(module, module.exports), module.exports;
 }
 
