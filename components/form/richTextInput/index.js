b3840ce3e8904bddf27e6168473344a8962a6d2d
mohsen parvar <mhsnprvrsec@gmail.com>
2020-09-28 19:55:20 +0330

    feat(Rich-Text):starting buttons implementation

--- components/form/richTextInput/index.js
+++ components/form/richTextInput/index.js
@@ -1,20 +1,41 @@
+import { useRef } from 'react'
 import { Controller } from 'react-hook-form'
 import { Editor } from 'draft-js'
+import styled, { css } from 'styled-components'
 
-export const RichTextInput = ({ control, name, setValue }) => {
+import { COMMON_INPUT_STYLES, ERROR_STYLE } from 'components'
+
+export const RichTextInput = ({ control, errors, name, setValue }) => {
+  const editor = useRef(null)
+
   return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ value, onChange }) => (
-        <Editor
-          editorState={value}
-          onChange={e => {
-            // console.log(convertToRaw(e.getCurrentContent()).blocks[0].text)
-            setValue(name, e)
-          }}
-        />
-      )}
-    />
+    <Wrapper errors={errors[name]} onClick={editor.current?.focus()}>
+      <Controller
+        name={name}
+        control={control}
+        render={({ value, onChange }) => (
+          <Editor
+            ref={editor}
+            editorState={value}
+            onChange={e => {
+              // console.log(convertToRaw(e.getCurrentContent()).blocks[0].text)
+              setValue(name, e)
+            }}
+          />
+        )}
+      />
+    </Wrapper>
   )
 }
+const Wrapper = styled.div`
+  height: 3rem;
+  div {
+    height: 100%;
+  }
+  ${props => COMMON_INPUT_STYLES(props)}
+  ${({ errors }) =>
+    errors &&
+    css`
+      ${props => ERROR_STYLE(props)}
+    `}
+`
