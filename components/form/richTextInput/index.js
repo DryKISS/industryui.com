0ad85a6fecd2f5de7dede9f132e77fe5dbd0ee71
Mohsen <mhsnprvr@gmail.com>
2020-10-12 14:34:44 +0330

    fix(RichTextInput):refactor and fix

--- components/form/richTextInput/index.js
+++ components/form/richTextInput/index.js
@@ -1,40 +1,42 @@
+// React
 import { useRef } from 'react'
-import { EditorState, convertToRaw, convertFromHTML, ContentState } from 'draft-js'
+import { object, string } from 'prop-types'
+
+import { ContentState, convertFromHTML, convertToRaw, EditorState } from 'draft-js'
 import { Controller } from 'react-hook-form'
 import { Editor } from 'react-draft-wysiwyg'
 import draftToHtml from 'draftjs-to-html'
 
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
-
-import { object, string } from 'prop-types'
-
+// UI
 import styled, { css } from 'styled-components'
-
 import { COMMON_INPUT_STYLES, ERROR_STYLE } from 'components'
+
 export const RichTextInput = ({ control, errors, initialValue, name }) => {
+  const editor = useRef(null)
+
   const content = EditorState.createWithContent(
     ContentState.createFromBlockArray(convertFromHTML(initialValue ?? ''))
   )
-  console.log(content)
-  const editor = useRef(null)
-  // const [editorState, setEditorState] = useState(EditorState.createEmpty())
-  const onEditorStateChange = (editorState, onChange) => {
-    console.log(editorState)
-    console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())))
-    onChange(editorState)
-    // onChange(draftToHtml(convertToRaw(editorState.getCurrentContent())))
-    // setEditorState(editorState)
-    // setValue(name, draftToHtml(convertToRaw(editorState.getCurrentContent())))
+
+  const defaultValue = {
+    editorState: content,
+    html: initialValue ?? ''
   }
+
+  const onEditorStateChange = (editorState, onChange) => {
+    const html = draftToHtml(convertToRaw(editorState.getCurrentContent()))
+    onChange({ editorState, html })
+  }
+
   return (
     <Wrapper errors={errors[name]} onClick={editor.current?.focus()}>
       <Controller
         name={name}
         control={control}
-        defaultValue={content}
+        defaultValue={defaultValue}
         render={({ value, onChange }) => (
           <Editor
-            editorState={value}
+            editorState={value.editorState}
             wrapperClassName='wrapper-class'
             editorClassName='editor-class'
             onEditorStateChange={e => onEditorStateChange(e, onChange)}
@@ -44,11 +46,8 @@ export const RichTextInput = ({ control, errors, initialValue, name }) => {
     </Wrapper>
   )
 }
+
 const Wrapper = styled.div`
-  /*height: 3rem;
-  div {
-    height: 100%;
-  }*/
   ${props => COMMON_INPUT_STYLES(props)}
   ${({ errors }) =>
     errors &&
@@ -56,6 +55,7 @@ const Wrapper = styled.div`
       ${props => ERROR_STYLE(props)}
     `}
 `
+
 RichTextInput.propTypes = {
   control: object.isRequired,
   errors: object.isRequired,
