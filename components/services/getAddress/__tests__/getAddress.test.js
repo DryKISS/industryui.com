a964effe334443dbf5ef000fb12aa99d24432b19
mohsen <mhsnprvr@gmail.com>
2020-10-20 13:30:26 +0200

    feat(GetAddress):continue on unit test

--- components/services/getAddress/__tests__/getAddress.test.js
+++ components/services/getAddress/__tests__/getAddress.test.js
@@ -19,4 +19,17 @@ describe('GetAddress Service', () => {
     expect(data.hasError).toEqual(false)
     expect.assertions(1)
   })
+
+  test('it should get the address array for postal code="SW19 2EZ" with a callback function', async () => {
+    const POSTAL_CODE = 'SW19 2EZ'
+    const callFunc = data => {
+      expect(data.hasError).toEqual(false)
+      expect.assertions(1)
+    }
+    GetAddressService.getAddresses({
+      callback: callFunc,
+      postCode: POSTAL_CODE,
+      callThrottle: 0
+    })
+  })
 })
