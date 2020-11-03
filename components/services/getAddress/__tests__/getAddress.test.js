52c49268d1495b15fea1572bfbae06c403bdaa6b
mohsen <mhsnprvr@gmail.com>
2020-11-03 14:01:46 +0100

    fix(getAddress):fixing tests

--- components/services/getAddress/__tests__/getAddress.test.js
+++ components/services/getAddress/__tests__/getAddress.test.js
@@ -12,13 +12,6 @@ describe('GetAddress Service', () => {
     expect(GetAddressService.fixAddresses(input)).toEqual(output)
   })
 
-  test('it should get the address array for postal code="SW19 2EZ"', async () => {
-    const POSTAL_CODE = 'SW19 2EZ'
-    const data = GetAddressService.getAddressesApi(null, POSTAL_CODE)
-    expect(data.hasError).toEqual(false)
-    expect.assertions(1)
-  })
-
   test('it should get the address array for postal code="SW19 2EZ" with a callback function', async () => {
     const POSTAL_CODE = 'SW19 2EZ'
     const callFunc = data => {
