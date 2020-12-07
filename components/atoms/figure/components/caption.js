c300372196b6284519e0c194438a649a91a561b1
komeilmehranfardrykiss <komeilmehranfar.drykiss@gmail.com>
2020-12-07 20:52:30 +0330

    fix(figure): add context and bg context to figure comp

--- components/atoms/figure/components/caption.js
+++ components/atoms/figure/components/caption.js
@@ -3,18 +3,19 @@
  */
 
 // React
-import { any, node, number, objectOf, oneOfType, string } from 'prop-types'
+import { CONTEXT } from 'components/theme'
+import { any, node, number, objectOf, oneOfType, string, oneOf } from 'prop-types'
 
 // Style
 import styled from 'styled-components'
 
-export const FigureCaption = ({ children, className, style }) => (
+export const FigureCaption = ({ children, className, style, context, bgContext }) => (
   <StyledCaption children={children} className={className} style={style} />
 )
 
 const StyledCaption = styled.figcaption`
-  background-color: ${({ theme }) => theme.COLOUR.white};
-  border: 1px solid ${({ theme }) => theme.COLOUR.dark};
+  color: ${({ context, theme: { COLOUR } }) => COLOUR[context]};
+  background-color: ${({ bgContext, theme: { COLOUR } }) => COLOUR[bgContext]};
   border-radius: 0 0 0.25rem 0.25rem;
   font-size: 90%;
   margin-top: -2px;
@@ -22,7 +23,14 @@ const StyledCaption = styled.figcaption`
 `
 
 FigureCaption.propTypes = {
+  bgContext: oneOf([Object.values(CONTEXT)]),
   children: node,
   className: any,
+  context: oneOf([Object.values(CONTEXT)]),
   style: objectOf(oneOfType([number, string]))
+}
+
+FigureCaption.defaultProps = {
+  bgContext: CONTEXT.DARK,
+  context: CONTEXT.WHITE
 }
