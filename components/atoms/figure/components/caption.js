290578ca2fba98869393379e577c6c30829e2520
Majid Amiri <majidamiriii@gmail.com>
2020-12-09 03:22:07 +0330

    fix(Figure): improve stories

--- components/atoms/figure/components/caption.js
+++ components/atoms/figure/components/caption.js
@@ -3,19 +3,29 @@
  */
 
 // React
-import { CONTEXT } from 'components/theme'
 import { any, node, number, objectOf, oneOfType, string, oneOf } from 'prop-types'
 
 // Style
 import styled from 'styled-components'
 
-export const FigureCaption = ({ children, className, style, context, bgContext }) => (
-  <StyledCaption children={children} className={className} style={style} />
-)
+// Components
+import { CONTEXT } from 'components/theme'
+
+export const FigureCaption = ({ children, className, style, context, bgContext }) => {
+  return (
+    <StyledCaption
+      bgContext={bgContext}
+      children={children}
+      className={className}
+      context={context}
+      style={style}
+    />
+  )
+}
 
 const StyledCaption = styled.figcaption`
-  color: ${({ context, theme: { COLOUR } }) => COLOUR[context]};
-  background-color: ${({ bgContext, theme: { COLOUR } }) => COLOUR[bgContext]};
+  color: ${({ context, theme }) => theme.COLOUR[context]};
+  background-color: ${({ bgContext, theme }) => theme.COLOUR[bgContext]};
   border-radius: 0 0 0.25rem 0.25rem;
   font-size: 90%;
   margin-top: -2px;
