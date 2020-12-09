e841f611c84cc5d803d9476ca851fe339bd6b36a
Majid Amiri <majidamiriii@gmail.com>
2020-12-10 01:56:07 +0330

    feat(Molecules/Tabs): add grabbale tabs

--- components/molecules/tabs/components/tabs.js
+++ components/molecules/tabs/components/tabs.js
@@ -3,8 +3,8 @@
  */
 
 // React
-import React, { useState } from 'react'
-import { array, bool, object, oneOfType, string } from 'prop-types'
+import React, { createRef, useEffect, useState } from 'react'
+import { array, bool, number, object, oneOfType, string } from 'prop-types'
 
 // Next
 import Router, { useRouter } from 'next/router'
@@ -13,10 +13,51 @@ import Router, { useRouter } from 'next/router'
 import { Tab } from './tab'
 
 // Style
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Tabs = ({ children, className, handleChange }) => {
+export const handleScroll = (el, grabWalkSpeed) => {
+  const slider = el
+  let isDown = false
+  let startX
+  let sl
+
+  slider.addEventListener('mousedown', e => {
+    isDown = true
+    startX = e.pageX - slider.offsetLeft
+    sl = slider.scrollLeft
+  })
+  slider.addEventListener('mouseleave', () => {
+    isDown = false
+    slider.classList.remove('active')
+  })
+  slider.addEventListener('mouseup', () => {
+    isDown = false
+    slider.classList.remove('active')
+  })
+  slider.addEventListener('mousemove', e => {
+    if (!isDown) return
+    e.preventDefault()
+    slider.classList.add('active')
+    const x = e.pageX - slider.offsetLeft
+    const walk = x - startX
+    slider.scrollLeft = sl - walk + grabWalkSpeed
+  })
+}
+
+export const Tabs = ({
+  children,
+  className,
+  grabbable,
+  grabWalkSpeed,
+  handleChange,
+  scrollToActiveTab
+}) => {
   const router = useRouter()
+  const wrapperRef = createRef()
+
+  useEffect(() => {
+    if (grabbable) handleScroll(wrapperRef.current, grabWalkSpeed)
+  }, [])
 
   if (!Array.isArray(children)) {
     children = React.Children.toArray(children)
@@ -38,23 +79,20 @@ export const Tabs = ({ children, className, handleChange }) => {
   }
 
   const handleTabChange = tab => {
-    // Debug
-    // console.log('Router change ', router)
-    // console.log('Router change2 ', router, router.query.id, router.query)
-
     const href = `${router.pathname}?id=${router.query.id}&tab=${tab}`
     Router.push(href, href, { shallow: true })
   }
 
   return (
     <>
-      <StyledTabs className={className}>
+      <StyledTabs className={className} grabbable={grabbable} ref={wrapperRef}>
         {children.map(({ props }) => {
           return (
             <Tab
               activeTab={activeTab}
               key={props.label}
               onClick={props.disabled ? () => {} : onClickTabItem}
+              scrollToActiveTab={scrollToActiveTab}
               {...props}
             />
           )
@@ -73,19 +111,45 @@ export const Tabs = ({ children, className, handleChange }) => {
 }
 
 const StyledTabs = styled.ol`
+  display: flex;
   align-items: flex-end;
   border-bottom: 1px solid ${({ theme }) => theme.TABS.borderColour};
-  display: flex;
   margin: 0 0 1rem 0;
   padding-left: 0;
+  white-space: nowrap;
+  overflow-x: scroll;
+  user-select: none;
+  -ms-overflow-style: none;
+  &&::-webkit-scrollbar {
+    display: none;
+  }
+
+  ${({ grabbable }) =>
+    grabbable &&
+    css`
+      &.active {
+        cursor: grabbing;
+
+        li {
+          cursor: grabbing;
+          pointer-events: none;
+        }
+      }
+    `}
 `
 
 Tabs.propTypes = {
   children: oneOfType([array, object]).isRequired,
   className: string,
-  handleChange: bool
+  grabbable: bool,
+  grabWalkSpeed: number,
+  handleChange: bool,
+  scrollToActiveTab: bool
 }
 
 Tabs.defaultProps = {
-  handleChange: true
+  grabbable: true,
+  grabWalkSpeed: 25,
+  handleChange: true,
+  scrollToActiveTab: true
 }
