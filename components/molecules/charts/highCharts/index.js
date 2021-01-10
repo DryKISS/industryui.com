8bef30d6b0e56b31be9c4ad97f3da205872fd84c
mohsen <mhsnprvr@gmail.com>
2021-01-10 08:13:24 +0100

    fix(highcharts):added highcharts props

--- components/molecules/charts/highCharts/index.js
+++ components/molecules/charts/highCharts/index.js
@@ -1,18 +1,61 @@
 /**
  * Molecules - Charts - Highcharts
  */
+import { any, arrayOf, bool, func, oneOf } from 'prop-types'
 
 // Highcharts
 import HighStockcharts from 'highcharts/highstock'
 import Highcharts from 'highcharts'
+import HighMapcharts from 'highcharts/highmaps'
+import HighGanttcharts from 'highcharts/highcharts-gantt'
 import HighchartsReact from 'highcharts-react-official'
 
-export const HighChart = ({ options, constructorType, chartType }) => {
+export const HighChart = ({
+  allowChartUpdate,
+  callback,
+  constructorType,
+  containerProps,
+  immutable,
+  options,
+  updateArgs,
+  ...props
+}) => {
+  let chart
+  switch (constructorType) {
+    case 'stockChart':
+      chart = HighStockcharts
+      break
+    case 'mapChart':
+      chart = HighMapcharts
+      break
+    case 'ganttChart':
+      chart = HighGanttcharts
+      break
+    default:
+      chart = Highcharts
+      break
+  }
   return (
     <HighchartsReact
-      highcharts={constructorType === 'stockChart' ? HighStockcharts : Highcharts}
+      allowChartUpdate={allowChartUpdate}
+      constructorType={constructorType || 'chart'}
+      highcharts={chart}
       options={options}
-      constructorType={constructorType}
+      callback={callback}
+      containerProps={containerProps}
+      immutable={immutable}
+      updateArgs={updateArgs}
+      {...props}
     />
   )
+}
+HighChart.prototypes = {
+  allowChartUpdate: bool,
+  callback: func,
+  constructorType: oneOf(['chart', 'stockChart', 'mapChart', 'ganttChart', undefined, null]),
+  containerProps: any,
+  highcharts: any,
+  immutable: bool,
+  options: any.isRequired,
+  updateArgs: arrayOf(bool)
 }
