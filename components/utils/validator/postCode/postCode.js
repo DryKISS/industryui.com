3ff36635a7ccab0128bb708773421e735d55b9ed
iwarner <ian.warner@drykiss.com>
2021-03-01 16:55:17 +0100

    fix(postcode): postcode validator fix for lint

--- components/utils/validator/postCode/postCode.js
+++ components/utils/validator/postCode/postCode.js
@@ -1,14 +1,16 @@
 /**
  * Components - Utils - Validator - Post Code
  */
+
+const r1 = `^([Gg][Ii][Rr] 0[Aa]{2})`
+const r2 = '|((([A-Za-z][0-9]{1,2})'
+const r3 = '|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])'
+const r4 = '|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\\s?[0-9][A-Za-z]{2})$'
+
+export const postCodeString = r1 + r2 + r3 + r4
+
 export const validatorPostCode = (postCode) => {
-  const r1 = `^([Gg][Ii][Rr] 0[Aa]{2})`
-  const r2 = '|((([A-Za-z][0-9]{1,2})'
-  const r3 = '|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])'
-  const r4 = '|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\\s?[0-9][A-Za-z]{2})$'
-
-  const re = new RegExp(r1 + r2 + r3 + r4)
-
+  const re = new RegExp(postCodeString)
   return re.test(postCode)
 }
 
