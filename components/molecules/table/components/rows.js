e99ef3b8664f2946f875de9fa7c4596966688d4f
mohsen <mhsnprvr@gmail.com>
2021-03-11 15:38:41 +0330

    fix(table):fixed object render

--- components/molecules/table/components/rows.js
+++ components/molecules/table/components/rows.js
@@ -3,80 +3,77 @@
  */
 
 // React
-import React, { memo, useRef, useState } from 'react'
+import React, { memo, useState } from 'react'
 import { array, bool, func, oneOfType, string } from 'prop-types'
 
 // UI
 import { TableData, TableRow } from '../../../'
 
-export const TableRows = memo(({ align, columns, hover, rowClick, rows, striped }) => {
-  const [selectedIndex, setSelectedIndex] = useState(null)
-  const handleClick = (e, index) => {
-    e.preventDefault()
-    const row = e.currentTarget.getAttribute('data-item')
-    rowClick(JSON.parse(row))
-    setSelectedIndex(index)
-  }
-
-  const clickable = typeof rowClick === 'function'
-
-  return rows.map((row, index) => {
-    const context = useRef(row.context)
-    delete row.context
-
-    if (row.hidden) {
-      return null
-    } else {
-      delete row.hidden
+export const TableRows = memo(
+  ({ align, changeSelectedRowBackground, columns, hover, rowClick, rows, striped }) => {
+    const [selectedIndex, setSelectedIndex] = useState(null)
+    const handleClick = (e, index) => {
+      e.preventDefault()
+      const row = e.currentTarget.getAttribute('data-item')
+      rowClick(JSON.parse(row))
+      setSelectedIndex(index)
     }
-    return (
-      <TableRow
-        context={context.current}
-        data-item={JSON.stringify(row)}
-        hover={hover}
-        key={index}
-        onClick={clickable ? (row) => handleClick(row, index) : null}
-        pointer={clickable}
-        selected={selectedIndex === index}
-        striped={striped}
-      >
-        {Object.entries(row).map(([key, value], index) => {
-          const length = columns.length
-          const column = columns[index]
-          const formatterData = column?.formatterData
-          if (length && column.hidden) {
-            return false
-          }
-          let formatter
-          if (formatterData) {
-            if (typeof formatterData === 'function') {
-              formatter = column.formatter({
-                row,
-                data: (row) => formatterData
-              })
-            } else {
-              formatter = column.formatter({ row, data: formatterData })
+
+    const clickable = typeof rowClick === 'function'
+
+    return rows.map((row, index) => {
+      const context = row.context
+
+      if (row.hidden) {
+        return null
+      } else {
+        delete row.hidden
+      }
+      return (
+        <TableRow
+          context={context}
+          data-item={JSON.stringify(row)}
+          hover={hover}
+          key={index}
+          onClick={clickable ? (row) => handleClick(row, index) : null}
+          pointer={clickable}
+          selected={changeSelectedRowBackground === true ? selectedIndex === index : false}
+          striped={striped}
+        >
+          {Object.entries(row).map(([key, value], index) => {
+            const length = columns.length
+            const column = columns[index]
+            const formatterData = column?.formatterData
+            if (length && column.hidden) {
+              return null
             }
-          }
 
-          const renderValue = typeof value === 'function' ? value() : value
+            const renderValue = typeof value === 'function' ? value() : value
 
-          return (
-            <TableData align={align} key={index}>
-              {length > 0 && column.formatter ? (
-                formatter
-              ) : value && value.__html ? (
-                <span dangerouslySetInnerHTML={value} />
-              ) : (
-                renderValue
-              )}
-            </TableData>
-          )
-        })}
-      </TableRow>
-    )
-  })
-})
+            return (
+              <TableData align={align} key={index}>
+                {length > 0 && column.formatter ? (
+                  typeof formatterData === 'function' ? (
+                    column.formatter({
+                      row,
+                      data: (row) => formatterData
+                    })
+                  ) : (
+                    column.formatter({ row, data: formatterData })
+                  )
+                ) : value && value.__html ? (
+                  <span dangerouslySetInnerHTML={value} />
+                ) : (
+                  renderValue
+                )}
+              </TableData>
+            )
+          })}
+        </TableRow>
+      )
+    })
+  }
+)
 
 TableRows.propTypes = {
   align: oneOfType([string, bool]),
