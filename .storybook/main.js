83593c84bd60bc379b8400d6a01839e90c36d4c9
iwarner <ian.warner@drykiss.com>
2021-05-06 11:57:35 +0200

    patch(storybook): changed the stories back to all

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,24 @@
-// module.exports = {
-//   stories:
-//     process.env.ENVIRONMENT === 'test'
-//       ? ['../components/**/__tests__/*stories.js', '../components/**/**.test.js']
-//       : ['../components/**/__tests__/*stories.js'],
-//   addons: [
-//     {
-//       name: '@storybook/addon-essentials'
-//     }
-//   ]
-// }
-
 module.exports = {
   stories:
     process.env.ENVIRONMENT === 'test'
-      ? ['../components/organisms/**/__tests__/*stories.js', '../components/organisms/**/**.test.js']
-      : ['../components/organisms/**/__tests__/*stories.js'],
+      ? ['../components/**/__tests__/*stories.js', '../components/**/**.test.js']
+      : ['../components/**/__tests__/*stories.js'],
   addons: [
     {
       name: '@storybook/addon-essentials'
     }
   ]
 }
+
+// module.exports = {
+//   stories:
+//     process.env.ENVIRONMENT === 'test'
+//       ? ['../components/organisms/**/__tests__/*stories.js', '../components/organisms/**/**.test.js']
+//       : ['../components/organisms/**/__tests__/*stories.js'],
+//   addons: [
+//     {
+//       name: '@storybook/addon-essentials'
+//     }
+//   ]
+// }
 
