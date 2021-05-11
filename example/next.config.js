ffc44ee23ba7a5703e9aa1f20ff697a94450eb05
mohsen <mhsnprvr@gmail.com>
2021-05-11 20:15:09 +0430

    feat(app):added analyzer

--- example/next.config.js
+++ example/next.config.js
@@ -2,15 +2,40 @@
  * Next Config
  */
 
-const withTM = require('next-transpile-modules')([
+ const withTM = require('next-transpile-modules')([
   '@fullcalendar/common',
   '@fullcalendar/daygrid',
   '@fullcalendar/list',
   '@fullcalendar/timegrid'
 ])
 
-module.exports = withTM({
-  future: {
-    webpack5: false
-  }
+const withPlugins = require('next-compose-plugins')
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
 })
+
+const nextConfig = {
+    future: {
+      webpack5: false
+    }
+  }
+
+module.exports = withPlugins([ withTM, withBundleAnalyzer], nextConfig)
+
+
+
+
+// module.exports = withBundleAnalyzer({})
+// const withTM = require('next-transpile-modules')([
+//   '@fullcalendar/common',
+//   '@fullcalendar/daygrid',
+//   '@fullcalendar/list',
+//   '@fullcalendar/timegrid'
+// ])
+
+// module.exports = withTM({
+//   future: {
+//     webpack5: false
+//   }
+// })
