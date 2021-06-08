14df39244873f8db46f7d3e63e74d14d6a8efb95
mohsen parvar <72754455+mohsenParvar@users.noreply.github.com>
2021-06-08 19:48:35 +0430

    fix(App):fixing imports

--- components/organisms/login/variables.js
+++ components/organisms/login/variables.js
@@ -3,7 +3,7 @@
  */
 
 // UI
-import { THEME_COLOUR } from '../../theme/variables/colour'
+import THEME_COLOUR from '../../constants/colour'
 
 export const LOGIN = {
   background: THEME_COLOUR.white
