2ced0e3ee1ed53c871f1953748bdf486087405f0
mohsenParvar <mhsnprvr@gmail.com>
2021-09-15 20:24:24 +0430

    fix(audioPlayer):minor refactor

--- components/molecules/audioPlayer/variables.js
+++ components/molecules/audioPlayer/variables.js
@@ -3,8 +3,7 @@ import THEME_COLOUR from '../../constants/colour'
 const AUDIO_PLAYER = {
   audioAndVolumeProgressColour: THEME_COLOUR.info,
   audioControllersColour: THEME_COLOUR.primary,
-  backgroundColour: THEME_COLOUR.light,
-
+  backgroundColour: THEME_COLOUR.light
 }
 
 export default AUDIO_PLAYER
