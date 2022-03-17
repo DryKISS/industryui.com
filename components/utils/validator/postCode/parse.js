4f60224ecf38e58c6c5b704ded518f2af5e37e1f
m.nasroalah <mehdi.barati@gmail.com>
2022-03-17 16:19:48 +0330

    fix(postcodeparse): fix postdoceParse function for other countries

--- components/utils/validator/postCode/parse.js
+++ components/utils/validator/postCode/parse.js
@@ -2,13 +2,16 @@
  * Components - Utils - Validator - Parse Post Parse
  */
 
+import POSTCODE_REGEXES from './regex'
+
 // Return area and district from Post Code
-const postCodeParse = (postCode) => {
-  const parsed = postCode?.match(/([A-Z]+)([0-9]+).*?[A-Z0-9]{3}/i) || ''
+const postCodeParse = ({ postCode, country }) => {
+  const regex = POSTCODE_REGEXES.get(country)
+  const parsed = postCode?.match(regex) || ''
 
   return {
     area: parsed[1],
-    district: parsed[2]
+    district: parsed && parsed.length > 2 ? parsed[2] : ''
   }
 }
 
