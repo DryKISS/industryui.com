4a63c607bc10d48db80d31574225a6484499e2d4
Ojaghi1991 <hosseinojaghi1991@gmail.com>
2022-04-07 13:23:54 +0430

    fix(FloatMenu): refactor onclick event

--- components/molecules/floatNav/helpers/generateMenu.js
+++ components/molecules/floatNav/helpers/generateMenu.js
@@ -1,11 +1,27 @@
 /**
  * Components - Molecules - Float Navbar - Helper - generateMenu
  */
+import React from 'react'
+
+const handleMenuClick = (e) => {
+  if (e.target !== e.currentTarget) return
+  for (let i = 0; i < e.target.children.length; i++) {
+    if (e.currentTarget.className.lastIndexOf('main') !== -1) {
+      // remove all main true
+      const mainChild = document.querySelector('.mainMenu').children[2]
+      for (let item = 0; item < mainChild.children.length; item++) {
+        mainChild.children[item].children[0].classList.remove('expand')
+        mainChild.children[item].classList.remove('active')
+      }
+    }
+    e.target.children[i].classList.toggle('expand')
+    li.classList.toggle('active')
+  }
+}
 
 const generateMenu = (container, menu, arrowDown) => {
   if (!menu || !menu.length) return
   const ul = document.createElement('ul')
-  ul.className = 'mainUl'
   let counter = -1
   for (const { title, submenus, href, main } of menu) {
     counter += 1
@@ -27,23 +43,7 @@ const generateMenu = (container, menu, arrowDown) => {
 
       generateMenu(li, submenus, true)
 
-      li.addEventListener('click', (e) => {
-        if (e.target !== e.currentTarget) return
-        for (let i = 0; i < e.target.children.length; i++) {
-          if (e.currentTarget.className.lastIndexOf('main') !== -1) {
-            // remove all main true
-            const mainChild = document.querySelector('.mainMenu').children[2]
-            console.log('mainChild :>> ', mainChild)
-            for (let item = 0; item < mainChild.children.length; item++) {
-              mainChild.children[item].children[0].classList.remove('expand')
-              mainChild.children[item].classList.remove('active')
-            }
-          }
-
-          e.target.children[i].classList.toggle('expand')
-          li.classList.toggle('active')
-        }
-      })
+      li.addEventListener('click', handleMenuClick)
     }
     ul.appendChild(li)
   }
