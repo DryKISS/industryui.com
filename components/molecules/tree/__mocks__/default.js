76ee0ececac81b69ef7f63df53d8bb63e9d3dcc2
Hossein Ojaghi <89131594+Ojaghi1991@users.noreply.github.com>
2022-04-26 15:13:12 +0430

    fix(Carousel): fix issue on tree component (#581)
    * fix(Carousel): fix issue on custom carousel pagination
    
    * fix(Tree): fix tree component design issue
    
    * fix(Tree): fix tree component issues on bullet
    
    * fix(Carousel): remove useless component on

--- components/molecules/tree/__mocks__/default.js
+++ components/molecules/tree/__mocks__/default.js
@@ -1,69 +1,71 @@
 export const treeData = [
   {
-    key: "0",
-    label: "Images",
-    icon: "images",
-    title: "Images Folder",
+    key: '0',
+    label: 'Images',
+    icon: 'images',
+    title: 'Images Folder',
     children: [
       {
-        key: "0-0",
-        label: "Document 1-1",
-        icon: "paperclip",
-        title: "Documents Folder",
+        key: '0-0',
+        label: 'Document 1-1',
+        icon: 'paperclip',
+        title: 'Documents Folder',
+        count: 3,
         children: [
           {
-            key: "0-1-1",
-            label: "Document-0-1.doc",
-            icon: "envelope",
-            title: "Documents Folder",
+            key: '0-1-1',
+            label: 'Document-0-1.doc',
+            icon: 'envelope',
+            title: 'Documents Folder'
           },
           {
-            key: "0-1-2",
-            label: "Document-0-2.doc",
-            icon: "fa fa-file",
-            title: "Documents Folder",
+            key: '0-1-2',
+            label: 'Document-0-2.doc',
+            icon: 'fa fa-file',
+            title: 'Documents Folder'
           },
           {
-            key: "0-1-3",
-            label: "Document-0-3.doc",
-            icon: "fa fa-file",
-            title: "Documents Folder",
+            key: '0-1-3',
+            label: 'Document-0-3.doc',
+            icon: 'fa fa-file',
+            title: 'Documents Folder'
           },
           {
-            key: "0-1-4",
-            label: "Document-0-4.doc",
-            icon: "fa fa-file",
-            title: "Documents Folder",
-          },
-        ],
-      },
-    ],
+            key: '0-1-4',
+            label: 'Document-0-4.doc',
+            icon: 'fa fa-file',
+            title: 'Documents Folder'
+          }
+        ]
+      }
+    ]
   },
   {
-    key: "1",
-    label: "Notifications",
-    icon: "bell",
-    title: "Notification Folder",
+    key: '1',
+    label: 'Notifications',
+    icon: 'bell',
+    title: 'Notification Folder',
     children: [
       {
-        key: "1-0",
-        label: "document1.doc",
-        icon: "fa fa-file",
-        title: "Documents Folder",
+        key: '1-0',
+        label: 'document1.doc',
+        icon: 'fa fa-file',
+        title: 'Documents Folder',
+        count: 4
       },
       {
-        key: "0-0",
-        label: "documennt-2.doc",
-        icon: "fa fa-file",
-        title: "Documents Folder",
-      },
-    ],
+        key: '0-0',
+        label: 'documennt-2.doc',
+        icon: 'fa fa-file',
+        title: 'Documents Folder',
+        count: 2
+      }
+    ]
   },
   {
-    key: "2",
-    label: "Trash",
-    icon: "trash",
-    title: "Downloads Folder",
-
-  },
-];
+    key: '2',
+    label: 'Trash',
+    icon: 'trash',
+    title: 'Downloads Folder'
+  }
+]
